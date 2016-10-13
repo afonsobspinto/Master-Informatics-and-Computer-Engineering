c8db717c9de4dbf2385f995a829b0617ba2aeec9
up201503316 <up201503316@f31db1e1-f3fa-489d-a1ec-45c04e6df4f4>
2016-10-13 17:46:09 +0000

    final
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g01@7 f31db1e1-f3fa-489d-a1ec-45c04e6df4f4

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,5 +1,9 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
+#include "i8254.h"
+
+int hook_id = 0;
+int counter = 0;
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
@@ -52,15 +56,25 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 
 int timer_subscribe_int(void ) {
 
-	return 1;
+	 if (sys_irqsetpolicy(TIMER0_IRQ,IRQ_REENABLE, &hook_id) == OK) // This function should be used to subscribe a notification on every interrupt in the input irq_line
+		if (sys_irqenable(&hook_id) == OK)
+		 return BIT(0);
+
+	return -1;
 }
 
 int timer_unsubscribe_int() {
 
-	return 1;
+	if (sys_irqdisable(&hook_id)==OK)
+		if (sys_irqrmpolicy(&hook_id)==OK) // This function unsubscribes a previous subscription of the interrupt notification associated with the specified hook_id
+			return 0;
+
+	return -1;
 }
 
 void timer_int_handler() {
+
+	counter += 1;
 
 }
 
@@ -83,26 +97,25 @@ int timer_get_conf(unsigned long timer, unsigned char *st) {
 			break;
 	}
 
-	sys_outb(TIMER_CTRL, command) // Ask the timer status to register control
+	sys_outb(TIMER_CTRL, command); // Ask the timer status to register control
 
 	switch (timer){
 
 			case 0:
-				sys_inb(TIMER_0, st) // Saves timer 0 info in st address
+				sys_inb(TIMER_0, st); // Saves timer 0 info in st address
 				return 0;
 
 			case 1:
-				sys_inb(TIMER_1, st) // Saves timer 1 info in st address
+				sys_inb(TIMER_1, st); // Saves timer 1 info in st address
 				return 0;
 
 			case 2:
-				sys_inb(TIMER_2, st) // Saves timer 2 info in st address
+				sys_inb(TIMER_2, st); // Saves timer 2 info in st address
 				return 0;
 		}
 
 	return 1;
 }
-
 
 int timer_display_conf(unsigned char conf) {
 
@@ -179,7 +192,42 @@ int timer_test_square(unsigned long freq) {
 
 int timer_test_int(unsigned long time) {
 
-	return 1;
+
+	int ipc_status;
+	message msg;
+	int r;
+	int freq = 60;
+
+	int irq_set=timer_subscribe_int();
+
+	printf("%x \n", irq_set);
+
+	while( counter < time * 60 ) { /* You may want to use a different condition */
+		/* Get a request message. */
+		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			 switch (_ENDPOINT_P(msg.m_source)) {
+			 case HARDWARE: /* hardware interrupt notification */
+				 if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+					 timer_int_handler();
+					 if ((counter % freq ==0))
+						 printf("Notification %d \n", counter/60);
+				 }
+				 break;
+			 default:
+				 break; /* no other notifications expected: do nothing */
+			 }
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+		}
+	}
+
+	timer_unsubscribe_int();
+
+	return 0;
 }
 
 int timer_test_config(unsigned long timer) {
