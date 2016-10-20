6469b0a9cf8e223ca95ccbcd8b83438a5fd69da8
up201503316 <up201503316@f31db1e1-f3fa-489d-a1ec-45c04e6df4f4>
2016-10-20 17:25:52 +0000

    keyword structure added for real
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g01@14 f31db1e1-f3fa-489d-a1ec-45c04e6df4f4

---
+++ lab3/keyword.c
@@ -0,0 +1,16 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include "i8254.h"
+
+int hook_id = 0;
+int counter = 0;
+
+int keyword_subscribe_int(void ) {
+
+	// Code goes here.
+}
+
+int keyword_unsubscribe_int() {
+
+	// Don't belive subscribe! Code goes here!
+}
