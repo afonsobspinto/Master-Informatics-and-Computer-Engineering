0ff1c377b4ef22b39aa6e9803eb3bdb736ab035f
Afonso Pinto <afonsobspinto@gmail.com>
2016-11-20 17:41:30 +0000

    main solved

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "interacao.h"
 #include "utils.h"
 #include "imovel.h"
-#include "menu.h"
+
 
 using namespace std;
 
@@ -50,23 +50,13 @@ int main() {
 
 		f.close();
 
-<<<<<<< HEAD
 		Broker Existente(nome, ficheiroClientes, ficheiroFornecedores, stof(receita_str));
-=======
-/*<<<<<<< HEAD
-		opcoesIniciais();
-
-//    	Broker Existente(nome, ficheiroClientes, ficheiroFornecedores, stof(receita_str));
-=======
-		Broker Existente(nome);
->>>>>>> 9bfbc4ff27a9d51b206920180d867d8c87afe78b
->>>>>>> 364338f8b37c1cd2adbd195e64f675f29dcaab2e
 
 		Existente.adicionaCliente();
 		Existente.adicionaFornecedor();
 
 		Existente.mostraMontra();
-*/
+
 
 	}
 	else{
