91e55c47b58d6eedaa89f55f0d7b60b63e7468d8
up201503316 <up201503316@f31db1e1-f3fa-489d-a1ec-45c04e6df4f4>
2016-12-01 23:56:56 +0000

    Hello World
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g01@52 f31db1e1-f3fa-489d-a1ec-45c04e6df4f4

---
+++ ChessProject/src/ChessProject.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(int argc, char **argv) {
+    printf("Hello, minix!\n");
+    return 0;
+}
---
+++ ChessProject/src/ChessProject.d
@@ -0,0 +1,8 @@
+
+ChessProject.o:	/usr/include/minix/dir.h
+ChessProject.o:	/usr/include/minix/types.h
+ChessProject.o:	/usr/include/sys/null.h
+ChessProject.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/ansi.h
+ChessProject.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/stdio.h
+ChessProject.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include/stdarg.h
+ChessProject.o:	ChessProject.c
