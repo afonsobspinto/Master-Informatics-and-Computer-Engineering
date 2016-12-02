bd2d78d0d5f3ab09b05ffbbf352fd190de16e822
up201503316 <up201503316@f31db1e1-f3fa-489d-a1ec-45c04e6df4f4>
2016-12-02 03:58:56 +0000

    Files from previous labs
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g01@53 f31db1e1-f3fa-489d-a1ec-45c04e6df4f4

--- ChessProject/src/ChessProject.c
+++ ChessProject/src/ChessProject.c
@@ -1,5 +1,41 @@
+#include "ChessProject.h"
+#include "utilities.h"
+#include "macros.h"
+
 #include <stdio.h>
+
+
 int main(int argc, char **argv) {
-    printf("Hello, minix!\n");
-    return 0;
+
+	/* Initialize service */
+	sef_startup();
+
+	/* Enable IO-sensitive operations for ourselves */
+	//sys_enable_iop(SELF);
+
+	if (chessproject_start())
+	{
+		chessproject_exit();
+		printf("ChessProject: An error occurred and the program was stopped.\n");
+		return 1;
+	}
+
+	chessproject_exit();
+
+	return 0;
+
+}
+
+int chessproject_start(){
+
+	vg_init(GRAF_1024x768);
+
+	sleep(2);
+
+	return 0;
+}
+
+int chessproject_exit(){
+	vg_exit();
+	return 0;
 }
--- ChessProject/src/ChessProject.d
+++ ChessProject/src/ChessProject.d
@@ -6,3 +6,6 @@ ChessProject.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/ansi.h
 ChessProject.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/stdio.h
 ChessProject.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include/stdarg.h
 ChessProject.o:	ChessProject.c
+ChessProject.o:	ChessProject.h
+ChessProject.o:	macros.h
+ChessProject.o:	utilities.h
