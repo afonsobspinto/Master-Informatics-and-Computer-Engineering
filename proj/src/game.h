a891d595b966e7228fafb925f1270afb6c91dc61
up201503316 <up201503316@f31db1e1-f3fa-489d-a1ec-45c04e6df4f4>
2016-12-29 05:10:09 +0000

    Backup
    git-svn-id: https://svn.fe.up.pt/repos/lcom1617-t1g01@98 f31db1e1-f3fa-489d-a1ec-45c04e6df4f4

---
+++ proj/src/game.h
@@ -0,0 +1,31 @@
+#ifndef __GAME_H
+#define __GAME_H
+
+#include "ChessProject.h"
+
+typedef enum {
+
+	WHITE2PLAY,
+	BLACK2PLAY,
+	PAUSED,
+	WHITEWINS,
+	BLACKWINS
+
+} GAME_STATE;
+
+MENU_STATE game_management();
+
+MENU_STATE menu_management();
+
+GAME_STATE getGameState();
+
+void turnGameState();
+
+void decrement(int *counter);
+
+void increment(int *counter);
+
+int waitForEnter();
+
+
+#endif /* __GAME_H */
