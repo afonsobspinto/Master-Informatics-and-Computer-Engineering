06187bfe9be5d57efbfbf0c404dc2647bf4962a7
Afonso Pinto <up201503316@fe.up.pt>
2017-02-17 12:35:16 +0000

    Task #2 UpdateDownMove

--- src/Board.java
+++ src/Board.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Board {
 	
 	public class Hero{
-		public int xPos;
-		public int yPos;
+		public int xPos = 1;
+		public int yPos = 1;
+		public int old_xPos = 1;
+		public int old_yPos = 1;
 	}
 	
 	Hero hero = new Hero();
+
+
 	
 	char [][] board = {
 			{'X', 'X', 'X', 'X','X','X','X','X','X','X'},
@@ -49,27 +53,53 @@ public class Board {
 			return -1;
 		}
 	}
-	
-	public void getHeroPos(){
+
+	public void applyLever(){
 		for(int i =0; i < 10; i++){
 			for(int j = 0; j < 10; j++){
-				if(board[i][j]=='H'){
-					hero.xPos = i;
-					hero.yPos = j;
-					break;
+				if(board[i][j]=='I'){
+					board[i][j] = 'S';
 				}
 			}
 		}
 	}
 	
-	public void play(){
-		int move = interaction(); //Le Movimento
-		
-		switch(move){
-		case 0:
-			break;
-		}
-		
+	public void updateBoard(){
 		
 	}
+	
+	public int play(){
+		int move = interaction(); //Read Move
+		
+		
+		switch(move){
+			
+		case 3:
+			if(hero.yPos+1 > 9) // Out of Board
+				return -1; // Move not Valid
+			if(board[hero.xPos][hero.yPos+1] == 'X' ||
+					board[hero.xPos][hero.yPos+1] == 'I'	) //Wall or Door
+				return 0; // Valido
+			if(board[hero.xPos][hero.yPos+1] == 'k'){ //Lever
+				hero.old_yPos = hero.yPos;
+				hero.yPos += 1;
+				applyLever();
+				updateBoard();
+				return 0;
+			}
+			if(board[hero.xPos][hero.yPos+2]=='G'){ // Guard
+				return 1; // Game Over
+				
+			}
+			if(board[hero.xPos][hero.yPos+2]==' '){ //Empty Cell
+				hero.old_yPos = hero.yPos;
+				hero.yPos += 1;
+				updateBoard();
+				return 0;
+			}		
+		}
+		return 0;
+	}
+	
+	
 }
--- src/DungeonKeep.java
+++ src/DungeonKeep.java
@@ -1,11 +1,12 @@
 import sun.print.resources.serviceui;
 
 public class DungeonKeep {
+	
 
 	public static void main(String[] args) {
 		Board x = new Board();
 		x.showBoard();
-		System.out.println(x.interaction());
-		
+		x.play();
+		//System.out.println(x.play());
 	}
 }
