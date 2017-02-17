cffb2318293edbc37db160e9646c5792991dc7b5
Afonso Pinto <up201503316@fe.up.pt>
2017-02-17 12:45:10 +0000

    Task #2 Old_Char bug solved

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Board {
 	
 	public class Hero{
@@ -7,6 +8,7 @@ public class Board {
 		public int yPos = 1;
 		public int old_xPos = 1;
 		public int old_yPos = 1;
+		public char old_char = ' ';
 	}
 	
 	Hero hero = new Hero();
@@ -65,40 +67,59 @@ public class Board {
 	}
 	
 	public void updateBoard(){
-		
+		board[hero.old_xPos][hero.old_yPos] = hero.old_char;
+		board[hero.xPos][hero.yPos] = 'H';
+		showBoard();
 	}
 	
 	public int play(){
+		int valid = -1;
 		int move = interaction(); //Read Move
+		
 		
 		
 		switch(move){
 			
 		case 3:
-			if(hero.yPos+1 > 9) // Out of Board
-				return -1; // Move not Valid
-			if(board[hero.xPos][hero.yPos+1] == 'X' ||
-					board[hero.xPos][hero.yPos+1] == 'I'	) //Wall or Door
-				return 0; // Valido
-			if(board[hero.xPos][hero.yPos+1] == 'k'){ //Lever
-				hero.old_yPos = hero.yPos;
-				hero.yPos += 1;
-				applyLever();
-				updateBoard();
-				return 0;
+			if(hero.xPos+1 > 9){ // Out of Board
+				valid = -1; // Move not Valid
+				break;
 			}
-			if(board[hero.xPos][hero.yPos+2]=='G'){ // Guard
-				return 1; // Game Over
+		
+			if(board[hero.xPos+1][hero.yPos] == 'X'){ //Wall 
+				hero.old_char = 'X';
+				valid = 0;
+				break; 
+			}
+			if(board[hero.xPos+1][hero.yPos] == 'I'){
+				hero.old_char = 'I';
+				valid = 0;
+				break; 
+			}
+			if(board[hero.xPos+1][hero.yPos] == 'k'){ //Lever
+				hero.old_xPos = hero.xPos;
+				hero.xPos += 1;
+				hero.old_char='k';
+				applyLever();
+				valid = 0;
+				break;
+			}
+			if(board[hero.xPos+2][hero.yPos]=='G'){ // Guard
+				valid = 1;  // Game Over
+				break;
 				
 			}
-			if(board[hero.xPos][hero.yPos+2]==' '){ //Empty Cell
-				hero.old_yPos = hero.yPos;
-				hero.yPos += 1;
-				updateBoard();
-				return 0;
+			if(board[hero.xPos+1][hero.yPos]==' '){ //Empty Cell
+				hero.old_xPos = hero.xPos;
+				hero.xPos += 1;
+				hero.old_char = ' ';
+				valid = 0;
+				break;
 			}		
 		}
-		return 0;
+		
+		updateBoard();
+		return valid;
 	}
 	
 	
