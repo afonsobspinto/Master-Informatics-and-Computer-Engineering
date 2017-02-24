ded4a0b11c2c687d35c9e7fb947aeca4d13c24c0
Afonso Pinto <up201503316@fe.up.pt>
2017-02-24 10:53:37 +0000

    Interation #2 Task #1.1 and #1.2 Complete

---
+++ src/console/Game.java
@@ -0,0 +1,46 @@
+package console;
+
+import gameLogic.*;
+
+public class Game {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		GameConfig gameConfig = new GameConfig();
+
+		int levels = 4;
+		int i = 1;
+		GameLogic g;
+
+		while(i <= levels){
+			g = new GameLogic(i, gameConfig);
+			
+			while(g.isGameOn()){
+				Interaction interaction = new Interaction(gameConfig);
+				Direction move = interaction.getDirection();
+				g.updateGame(i,move);
+				g.showBoard();
+				
+				if(!g.isGameOn() && g.isWon()){
+					System.out.println("\n\n\n\n");
+					System.out.println("Victory");
+					System.out.println("\n\n\n\n");
+					i++;
+				}
+				else if(!g.isGameOn() && !g.isWon()){
+					System.out.println("\n\n\n\n");
+					System.out.println("Defeat");
+					System.out.println("\n\n\n\n");
+					break;
+					
+				}
+				
+			}
+					
+				
+		}
+
+	}
+
+}
---
+++ src/gameLogic/GameLogic.java
@@ -0,0 +1,105 @@
+
+package gameLogic;
+
+import java.util.Scanner;
+import console.Interaction;
+
+public class GameLogic {
+
+	private Board board;
+	private Hero hero;
+	private Guard guard;
+	private CrazyOgre crazyOgre;
+	private GameConfig gameConfig;
+	boolean won;
+	boolean gameOn;
+
+	public GameLogic(int level, GameConfig gameConfig){
+
+		this.gameConfig = gameConfig;
+		this.gameOn = true;
+		this.won = false;
+
+		this.board = new Board(level);
+		this.hero = new Hero(level);
+		this.guard = new Guard(level);
+		this.crazyOgre= new CrazyOgre(level);
+
+		this.board.setBoardAt(hero.position, hero.symbol);
+		this.board.setBoardAt(guard.position, guard.symbol);
+		this.board.setBoardAt(crazyOgre.position, crazyOgre.symbol);
+		if(crazyOgre.isArmed)
+			this.board.setBoardAt(crazyOgre.weaponLocation, crazyOgre.weapon);
+		
+		this.board.showBoard();
+		
+
+	}
+	
+	
+	public void applyLever(){
+		for(int i =0; i < gameConfig.getrows(); i++){
+			for(int j = 0; j < gameConfig.getcolumns(); j++){
+				if(this.board.getBoardAt(i, j)=='I'){
+					this.board.setBoardAt(i,j, 'S');
+				}
+			}
+}
+	}
+	
+
+	public void updateGame(int level, Direction move){
+
+		guard.move(board);
+		crazyOgre.move(board);
+		Action action = hero.move(this.board, move);
+
+		if(hero.isSymbolnearby(board, 'G')){
+			action = Action.GUARD;
+		}
+		if(hero.isSymbolnearby(board, 'O') || hero.isSymbolnearby(board, '$') || hero.isSymbolnearby(board, '*')){
+			action = Action.CRAZYOGRE;
+		}
+
+
+		switch (action) {
+		case NOACTION:
+			break;
+		case GUARD:
+			this.won = false;
+			gameOn = false;
+			break;
+		case CRAZYOGRE:
+			this.won = false;
+			gameOn = false;
+			break;
+		case OPENDOOR:
+			this.won= true;
+			gameOn= false;
+			break;
+		case LEVER:
+			applyLever();
+		case MOVE:
+			break;
+		case KEY:
+			break;
+
+		default:
+			break;
+		}
+	}
+
+
+	public boolean isWon() {
+		return won;
+	}
+	
+	public boolean isGameOn() {
+		return gameOn;
+	}
+	
+	public void showBoard(){
+		board.showBoard();
+	}
+}
+
