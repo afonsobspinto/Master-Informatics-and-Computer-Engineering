d5a4aa7ad6d973ebc52d1a7878d3f3a6cb22d3ca
Tomás <toliveira_97@hotmail.com>
2017-03-05 15:06:02 +0000

    JUnit Error?

--- src/tests/dungeonTest.java
+++ src/tests/dungeonTest.java
@@ -1,5 +1,26 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import gameLogic.Coord;
+import gameLogic.Direction;
+import gameLogic.GameConfig;
+import gameLogic.GameLogic;
+
 public class dungeonTest {
 
+	@Test
+	public void testMoveHeroIntoToFreeCell(){
+		
+		System.out.println("test");
+		GameConfig game = new GameConfig();
+		GameLogic g = new GameLogic(1, game);
+		g.updateGame(1,Direction.RIGHT);
+		g.showBoard();
+		assertEquals(new Coord(1,2), g.getHero().getPosition());
+	}
+
 }
+
+
