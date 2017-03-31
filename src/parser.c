40ddfc79aef260d326afcacb30f26713eb38929a
FooWalksIntoABar <up201503316@fe.up.pt>
2017-03-31 18:53:09 +0100

    endOfClass

--- src/parser.c
+++ src/parser.c
@@ -4,6 +4,10 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
 int parser(int argc, char *argv[])
 {
@@ -11,6 +15,8 @@ int parser(int argc, char *argv[])
 	struct dirent *direntp;
 	struct stat stat_buf;
 	char *str;
+	pid_t pid;
+
 	if (argc != 2)
 	{
 		fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
@@ -24,11 +30,27 @@ int parser(int argc, char *argv[])
 	while ((direntp = readdir( dirp)) != NULL)
 	{
 		lstat(direntp->d_name, &stat_buf);
-		if (S_ISREG(stat_buf.st_mode)) str = "regular";
-		else if (S_ISDIR(stat_buf.st_mode)) str = "directory";
-		else str = "other";
+		if (S_ISREG(stat_buf.st_mode)){ // Is this the file I'm looking for?
+			str = "regular";
+		}
+		else if (S_ISDIR(stat_buf.st_mode)){ // Fork Here
+			str = "directory";
+
+			if((pid=fork())<0){
+				 fprintf(stderr,"fork error\n");
+			}
+			 else if (pid == 0) { //Update Dir & Recall Function
+			 }
+
+		}
+		else{
+			str = "other";
+		}
 		printf("%-25s - %s\n", direntp->d_name, str);
 	}
 	closedir(dirp);
+
+
+	waitpid(-1,NULL,0); //Make parent wait for all Childs (do it after find the cena)
 	exit(0);
 }
