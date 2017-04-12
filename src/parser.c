2263a125be9f3cd7c6465e38d117e80879d22c14
Tomás <toliveira_97@hotmail.com>
2017-04-13 00:10:14 +0100

    Permissions

--- src/parser.c
+++ src/parser.c
@@ -108,7 +108,8 @@ bool isValidFile(const struct stat* statBuf, const struct dirent *direntp, const
 			return false;
 		}
 	}
-	//if((args->perm != 0))
+	if((args->perm != 0) && ((buf.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO) == args->perm))
+			return true;
 
 	return false;
 }
