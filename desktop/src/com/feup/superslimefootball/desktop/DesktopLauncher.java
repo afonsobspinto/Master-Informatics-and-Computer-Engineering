23c3c59c4afd1e76c018983fa132a811ff85ce1f
FooWalksIntoABar <up201503316@fe.up.pt>
2017-04-21 12:08:38 +0100

    Initial commit

---
+++ desktop/src/com/feup/superslimefootball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.feup.superslimefootball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.feup.superslimefootball.superSlimeFootball;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new superSlimeFootball(), config);
+	}
+}
---
+++ html/src/com/feup/superslimefootball/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.feup.superslimefootball.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.feup.superslimefootball.superSlimeFootball;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new superSlimeFootball();
+        }
+}
