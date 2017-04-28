02c1801537c911f7338b058442bb400a9999a577
FooWalksIntoABar <up201503316@fe.up.pt>
2017-04-28 01:03:55 +0100

    starting from scratch with MVC pattern

--- core/src/com/feup/superslimefootball/superSlimeFootball.java
+++ core/src/com/feup/superslimefootball/superSlimeFootball.java
@@ -6,29 +6,21 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import states.GameStateManager;
-import states.MainMenuState;
-
 
 public class superSlimeFootball extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture img;
-	private GameStateManager gsm;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		gsm = new GameStateManager();
 		img = new Texture("badlogic.jpg");
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		gsm.push(new MainMenuState(gsm));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
 	}
 	
 	@Override
--- core/src/entities/Player.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-/**
- * Created by afonso on 4/23/17.
- */
-
-public final class Player {
-}
