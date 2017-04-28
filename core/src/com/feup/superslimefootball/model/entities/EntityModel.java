e5979b8140c7fc41279b31d48a031f9c2731fce2
Tomás <toliveira_97@hotmail.com>
2017-04-28 11:47:53 +0100

    Model Entities

--- core/src/com/feup/superslimefootball/model/entities/EntityModel.java
+++ core/src/com/feup/superslimefootball/model/entities/EntityModel.java
@@ -1,8 +1,28 @@
 package com.feup.superslimefootball.model.entities;
 
 /**
- * Created by afonso on 4/28/17.
+ * An abstract model representing an entity belonging to a game model.
  */
 
-public class EntityModel {
+public abstract class EntityModel {
+    /**
+     * The x-coordinate of this model in meters.
+     */
+    private float x;
+
+    /**
+     * The y-coordinate of this model in meters.
+     */
+    private float y;
+
+    /**
+     * Constructs a model with a position.
+     *
+     * @param x The x-coordinate of this entity in meters.
+     * @param y The y-coordinate of this entity in meters.
+     */
+    EntityModel(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
