265eb20dc2790c4c4501fa4c879c9fc19a9b2f80
FooWalksIntoABar <up201503316@fe.up.pt>
2017-04-30 21:14:41 +0100

    network and states package

---
+++ core/src/com/feup/superslimefootball/model/states/GamePlayStateModel.java
@@ -0,0 +1,11 @@
+package com.feup.superslimefootball.model.states;
+
+/**
+ * Created by afonso on 4/30/17.
+ */
+
+public class GamePlayStateModel extends StateModel {
+    public GamePlayStateModel(GameStateManagerModel gsm) {
+        super(gsm);
+    }
+}
---
+++ core/src/com/feup/superslimefootball/model/states/MainMenuStateModel.java
@@ -0,0 +1,11 @@
+package com.feup.superslimefootball.model.states;
+
+/**
+ * Created by afonso on 4/30/17.
+ */
+
+public class MainMenuStateModel extends StateModel {
+    public MainMenuStateModel(GameStateManagerModel gsm) {
+        super(gsm);
+    }
+}
