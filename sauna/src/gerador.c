a88b18e977447743399220d2cbf6967a2b6449f4
FooWalksIntoABar <up201503316@fe.up.pt>
2017-05-11 23:46:56 +0100

    start with ./gerador <n. pedidos> <max. utilização>

--- sauna/src/gerador.c
+++ sauna/src/gerador.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char* argv[], char* envp[]){
 
-	printf("Hello World Gerador \n");
+
+	if(argc != 3){
+		printf("Wrong number of arguments \n.");
+		exit(1);
+	}
+
+	unsigned int numberOrders;
+	unsigned int usageTime;
+
+	if((numberOrders = atoi(argv[1])) == 0){
+		printf("Invalid number of orders. \n");
+		exit(1);
+	}
+
+	if((usageTime = atoi(argv[2])) == 0){
+		printf("Invalid usage time. \n");
+		exit(1);
+	}
+
+	printf("Number of Orders: %d \nUsage Time: %d \n", numberOrders, usageTime);
 
 	return 0;
+
+
 }
