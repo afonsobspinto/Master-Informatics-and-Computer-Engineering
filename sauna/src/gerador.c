766b5ccc120b564b1f059afe7d835dffcb7b786b
FooWalksIntoABar <up201503316@fe.up.pt>
2017-05-11 23:31:08 +0100

    makefile

--- sauna/src/gerador.c
+++ sauna/src/gerador.c
@@ -2,7 +2,7 @@
 
 int main (int argc, char* argv[], char* envp[]){
 
-	printf("Hello World Sauna");
+	printf("Hello World Gerador \n");
 
 	return 0;
 }
---
+++ sauna/src/sauna.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[], char* envp[]){
+
+	printf("Hello World Sauna \n");
+
+	return 0;
+}
