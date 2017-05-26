dd48db669114c3208a19c36d80dca7a9b31a9ade
FooWalksIntoABar <up201503316@fe.up.pt>
2017-05-26 15:24:30 +0100

    restarting

--- core/src/com/feup/superslimefootball/SuperSlimeFootball.java
+++ core/src/com/feup/superslimefootball/SuperSlimeFootball.java
@@ -1,81 +1,27 @@
 package com.feup.superslimefootball;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.feup.superslimefootball.view.GameView;
 
-
-/**
- * The game main class.
- */
-
-public class SuperSlimeFootball extends Game {
-	private SpriteBatch batch;
-	private AssetManager assetManager;
-
-    /**
-     * Creates the game. Initializes the sprite batch and asset manager.
-     * Also starts the game until we have a main menu.
-     */
+public class SuperSlimeFootball extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture img;
 
     @Override
-    public void create() {
+    public void create () {
         batch = new SpriteBatch();
-        assetManager = new AssetManager();
-
-        startGame();
+        img = new Texture("badlogic.jpg");
     }
 
-    /**
-     * Starts the game.
-     */
-    private void startGame() {
-        setScreen(new GameView(this));
-    }
-
-    /**
-     * Disposes of all assets.
-     */
-
-	@Override
-	public void dispose() {
-		batch.dispose();
-        assetManager.dispose();
-	}
-
-	//TODO: pause
-
-	@Override
-	public void pause() {
-		super.pause();
-	}
-
-	//TODO: resume
-
-	@Override
-	public void resume() {
-		super.resume();
-	}
-
-    /**
-     * Returns the sprite batch used to improve drawing performance.
-     *
-     * @return the sprite batch
-     */
-
-    public SpriteBatch getBatch() {
-        return batch;
-    }
-
-    /**
-     * Returns the asset manager used to load all textures and sounds.
-     *
-     * @return the asset manager
-     */
-
-    public AssetManager getAssetManager() {
-
-        return assetManager;
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
     }
 }
