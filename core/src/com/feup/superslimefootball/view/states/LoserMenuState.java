d48fdb5809cb0c34f169ffbc9d2ecaf90676d720
Tomás <toliveira_97@hotmail.com>
2017-06-03 11:14:14 +0100

    Winner and Loser Menu can go back to menu now

--- core/src/com/feup/superslimefootball/view/states/LoserMenuState.java
+++ core/src/com/feup/superslimefootball/view/states/LoserMenuState.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.feup.superslimefootball.SuperSlimeFootball;
 
-public class LoserMenuState extends MenuState { //Tomás Menu xD
+public class LoserMenuState extends MenuState {
     public LoserMenuState(SuperSlimeFootball game) {
         super(game);
     }
@@ -15,16 +15,20 @@ public class LoserMenuState extends MenuState { //Tomás Menu xD
         Texture facebook = this.game.getAssetManager().get("facebook.png", Texture.class);
         Texture twitter = this.game.getAssetManager().get("twitter.png", Texture.class);
         Texture youlost = this.game.getAssetManager().get("expressions/Sorryyoulost.png", Texture.class);
+        Texture goBack = this.game.getAssetManager().get("goBack.png", Texture.class);
 
         this.game.getBatch().draw(facebook, Gdx.graphics.getWidth()*(12.0f/13.0f), Gdx.graphics.getHeight()*(17.0f/20.0f));
         this.game.getBatch().draw(twitter, Gdx.graphics.getWidth()*(12.0f/13.0f), Gdx.graphics.getHeight()*(14.0f/20.0f));
         this.game.getBatch().draw(youlost, Gdx.graphics.getWidth()*(3.0f/10.0f), Gdx.graphics.getHeight()*(14.0f/20.0f));
+        this.game.getBatch().draw(goBack, Gdx.graphics.getWidth()*(1.0f/13.0f), Gdx.graphics.getHeight()*(17.0f/20.0f));
 
     }
 
-
     @Override
     public void handleMouse() {
-
+        if(Gdx.input.justTouched()) {
+            if (touchButton(1.0f / 13.0f, 17.0f / 20.0f, BUTTONS_WIDTH, BUTTONS_HEIGHT))
+                this.game.setGameState(new InitialMenuState(this.game));
+        }
     }
 }
