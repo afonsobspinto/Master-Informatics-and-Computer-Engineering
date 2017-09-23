9f306f08f6d70c1c96b5115e337c04e51ba81a2c
Afonso Pinto <up201503316@fe.up.pt>
2017-09-23 18:09:07 +0100

    exercise 1

---
+++ exercises/pl_exercises_1/ex1.pl
@@ -0,0 +1,24 @@
+% --- male list --- %
+
+male(aldo).
+male(lincoln).
+male(lj).
+male(michael).
+
+% --- female list --- %
+
+female(christina).
+female(lisa).
+female(sara).
+female(ella).
+
+% --- parents list --- %
+
+parent(aldo, lincoln).
+parent(aldo, michael).
+parent(christina, lincoln).
+parent(christina, michael).
+parent(lincoln, lj).
+parent(michael, ella).
+parent(lisa, lj).
+parent(sara, ella).
