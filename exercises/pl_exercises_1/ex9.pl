3cc408603f32d2d7c22f3a448bcd01d8ce8ce26d
Afonso Pinto <up201503316@fe.up.pt>
2017-09-25 12:10:01 +0100

    exercise 0

---
+++ exercises/pl_exercises_1/ex9.pl
@@ -0,0 +1,19 @@
+% --- facts list --- %
+
+aluno(joao, paradigmas).
+aluno(maria, paradigmas).
+aluno(joel, lab2).
+aluno(joel, estruturas).
+frequenta(joao, feup).
+frequenta(maria, feup).
+frequenta(joel, ist).
+professor(carlos, paradigmas).
+professor(ana_paula, estruturas).
+professor(pedro, lab2).
+funcionario(pedro, ist).
+funcionario(ana_paula, feup).
+funcionario(carlos, feup).
+
+% --- rules list --- %
+
+isStudentOf(X, A):- aluno(A, D), professor(X,D).
