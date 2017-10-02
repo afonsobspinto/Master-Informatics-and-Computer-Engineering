21e0500d0516b4965fea6f48f4b6665b698a89f6
Afonso Pinto <up201503316@fe.up.pt>
2017-10-02 16:29:42 +0100

    Create writenoncanonical.c

---
+++ Aula2/writenoncanonical.c
@@ -0,0 +1,123 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> // for close
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+#define FLAG 0x7E
+#define A 0x03
+#define C 0x03
+
+volatile int STOP=FALSE;
+
+enum state { start, flagRCV, aRCV, cRCV, bccOK, stop};
+
+
+int main(int argc, char** argv)
+{
+    int fd,res;
+    struct termios oldtio,newtio;
+
+    unsigned char SET[5];
+    unsigned char UA[5];
+
+    char buf[255];
+    char readBuf[255];
+    int i, sum = 0, speed = 0;
+
+    enum state actualState;
+    actualState = start;
+
+
+    if ( (argc < 2) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+
+    /**
+     Init the SET buffer
+     **/
+
+    set[0] = FLAG;
+    set[1] = A;
+    set[2] = C_SET;
+    set[3] = set[1]^set[2];
+    set[4] = FLAG
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+
+      gets(buf);
+      int bufLength = strlen(buf) + 1;
+      res = write(fd,buf,bufLength);
+      printf("%d bytes written\n", res);
+
+      //read from serial port
+      int j = 0;
+      char chr;
+      while(STOP==FALSE) {
+        res = read(fd,&chr,1);
+        if (res == -1) {
+          perror("Error on reading");
+          exit(-1);
+        }
+        readBuf[j++] = chr;
+        if (chr=='\0') STOP=TRUE;
+      }
+      printf("Read %s, Length:%d bytes\n", readBuf, (int)strlen(readBuf));
+
+
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+
+    close(fd);
+    return 0;
+}
