d650cfd2e23ab86999e8b53a0db1ee1150f085a3
Tomás Oliveira <up201504746@fe.up.pt>
2017-11-10 18:19:03 +0000

    Menus

--- Project1/src/menus.pl
+++ Project1/src/menus.pl
@@ -3,8 +3,8 @@ mainMenu:-
   getChar(Char),
   (
 		Char = '1' -> gameMenu, mainMenu;
-		Char = '2' -> mainMenu;
-		Char = '3' -> mainMenu;
+		Char = '2' -> howToPlayMenu;
+		Char = '3' -> aboutMenu;
 		Char = '4';
 		nl,
 		write('Error: invalid input.'), nl,
@@ -14,16 +14,79 @@ mainMenu:-
 
 printMainMenu:-
   clearConsole,
-  write('   Racing Kings                '), nl, nl,
+  write(' ______________________________________________________________________________ '),nl,
+  write('|                                                                              |'),nl,
+  write('|               Racing Kings                                                   |'),nl,
+  write('|                                                                              |'),nl,
+  write('|                                                                              |'),nl,
+  write('| 1. Play                                                                      |'),nl,
+  write('| 2. How to Play                                                               |'),nl,
+  write('| 3. About                                                                     |'),nl,
+  write('| 4. Exit                                                                      |'),nl,
+  write('|                                                                              |'),nl,
+  write('| Choose an option:                                                            |'),nl,
+  write('|______________________________________________________________________________|'),nl,
+  nl, nl.
+  
+howToPlayMenu:-
+  printHowToPlayMenu,
+  getChar(Char),
+  (
+		Char = '1' -> mainMenu;
+		Char = '2';
+		nl,
+		write('Error: invalid input.'), nl,
+		pressEnterToContinue, nl,
+		mainMenu
+).
 
-  write('   1. Play                     '), nl,
-  write('   2. How to play              '), nl,
-  write('   3. About                    '), nl,
-  write('   4. Exit                     '), nl,nl,
+printHowToPlayMenu:-
+  clearConsole,
+  write(' ______________________________________________________________________________ '),nl,
+  write('|                                                                              |'),nl,
+  write('|     How to Play?                                                             |'),nl,
+  write('|                                                                              |'),nl,
+  write('|                                                                              |'),nl,
+  write('| The purpose of the game is to be the first player to move his king           |'),nl,
+  write('| to the eight row. When whoevers playing white moves their king to            |'),nl,
+  write('| the eight row, and the one playing black moves their king to the last        |'), nl,
+  write('| row as well, directly after that, the game ends in a draw.                   |'),nl,
+  write('| In this game, check is entirely forbidden, not only is it forbidden          |'),nl,
+  write('| to move ones king into check, but it is also forbidden to check the          |'),nl,
+  write('| opponents king.                                                              |'),nl,
+  write('|                                                                              |'),nl,
+  write('| 1. Back                                                                      |'),nl,
+  write('| 2. Exit                                                                      |'),nl,
+  write('|______________________________________________________________________________|'),nl,
+  nl, nl.
 
-  write(' Choose an option:'), nl.
+aboutMenu:-
+  printAboutMenu,
+  getChar(Char),
+  (
+		Char = '1' -> mainMenu;
+		Char = '2';
+		nl,
+		write('Error: invalid input.'), nl,
+		pressEnterToContinue, nl,
+		mainMenu
+).
 
-
+printAboutMenu:-
+  clearConsole,
+  write(' ______________________________________________________________________________ '),nl,
+  write('|                                                                              |'),nl,
+  write('|     About                                                                    |'),nl,
+  write('|                                                                              |'),nl,
+  write('|                                                                              |'),nl,
+  write('| The game is a variant of Chess, invented by Vernan R. Parton.                |'),nl,
+  write('| Adapted to PROLOG by Afonso Pinto and Tomás Oliveira.                        |'),nl,
+  write('|                                                                              |'),nl,
+  write('| 1. Back                                                                      |'),nl,
+  write('| 2. Exit                                                                      |'),nl,
+  write('|______________________________________________________________________________|'),nl,
+  nl, nl.
+  
 gameMenu:-
   	printGameMenu,
   	getChar(Char),
@@ -31,7 +94,7 @@ gameMenu:-
   		Char = '1' -> startPvPGame;
   		Char = '2';
   		Char = '3';
-      Char = '4';
+        Char = '4';
 
   		nl,
   		write('Error: invalid input.'), nl,
@@ -40,14 +103,20 @@ gameMenu:-
   	).
 
 printGameMenu:-
-    	clearConsole,
-    	write('   Racing Kings                '), nl, nl,
-    	write('   1. Player vs. Player        '), nl,
-    	write('   2. Player vs. Computer      '), nl,
-    	write('   3. Computer vs. Computer    '), nl,
-    	write('   4. Back                     '), nl,nl,
-
-      write(' Choose an option:'), nl.
+  clearConsole,
+  write(' ______________________________________________________________________________ '),nl,
+  write('|                                                                              |'),nl,
+  write('|               Racing Kings                                                   |'),nl,
+  write('|                                                                              |'),nl,
+  write('|                                                                              |'),nl,
+  write('| 1. Player vs Player                                                          |'),nl,
+  write('| 2. Player vs Computer                                                        |'),nl,
+  write('| 3. Computer vs Computer                                                      |'),nl,
+  write('| 4. Back                                                                      |'),nl,
+  write('|                                                                              |'),nl,
+  write('| Choose an option:                                                            |'),nl,
+  write('|______________________________________________________________________________|'),nl,
+  nl, nl.
 
 startPvPGame:-
     createPvPGame(Game),
