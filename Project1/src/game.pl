f156b082f908e758d06abdf4d1945b7a88dd846a
Tomás Oliveira <up201504746@fe.up.pt>
2017-11-11 21:14:20 +0000

    clearConsole on InvalidMove and random on CvC

--- Project1/src/game.pl
+++ Project1/src/game.pl
@@ -28,7 +28,11 @@ createPvCGame(Game):-
 
 createCvCGame(Game):-
 	initialBoard(Board),
-	Game = [Board, whiteToMove, cvc], !,
+	random(0,2,Color),
+	(
+		Color == 0, Game = [Board, whiteToMove, cvcWhite], !;
+		Game = [Board, whiteToMove, cvcBlack], !
+	),
 	bb_put(blackCanTieFlag, 0).
 
 
@@ -67,9 +71,13 @@ playGame(Game):-
 			getBoard(Game, Board), clearConsole, printBoard(Board), printGameInfo(Game), nl,nl, pressEnterToContinue, somehowSmartBotTurn(Game, ContinueGame), %TODO: RandomBot Opponent
 			getBoard(ContinueGame, ContinueBoard), clearConsole, printBoard(ContinueBoard), printGameInfo(ContinueGame), humanTurn(ContinueGame, HumanContinueGame),
 			playGame(HumanContinueGame), !);
-		GameMode == cvc -> (
+		GameMode == cvcWhite -> (
 			getBoard(Game, Board), clearConsole, printBoard(Board), printGameInfo(Game), nl,nl, pressEnterToContinue, botTurn(Game, ContinueGame),
 			getBoard(ContinueGame, ContinueBoard), clearConsole, printBoard(ContinueBoard), printGameInfo(ContinueGame), nl,nl, pressEnterToContinue, somehowSmartBotTurn(ContinueGame, BotContinueGame),
+			playGame(BotContinueGame), !);
+		GameMode == cvcBlack -> (
+			getBoard(Game, Board), clearConsole, printBoard(Board), printGameInfo(Game), nl,nl, pressEnterToContinue, somehowSmartBotTurn(Game, ContinueGame),
+			getBoard(ContinueGame, ContinueBoard), clearConsole, printBoard(ContinueBoard), printGameInfo(ContinueGame), nl,nl, pressEnterToContinue, botTurn(ContinueGame, BotContinueGame),
 			playGame(BotContinueGame), !)
 	).
 
@@ -202,7 +210,7 @@ validateOwnership(_, _, _):-
 
 
 validateMove(SrcCol, SrcRow, DestCol, DestRow, Board, Flag):-
-	differentPositions(SrcCol, SrcRow, DestCol, DestRow, Flag), !,
+	differentPositions(SrcCol, SrcRow, DestCol, DestRow, Board, Flag), !,
 	differentColors(SrcCol, SrcRow, DestCol, DestRow, Board, Flag), !,
 	getPiece(Board, SrcCol, SrcRow, Piece),
 	getPieceName(Piece, PieceName),
@@ -211,11 +219,11 @@ validateMove(SrcCol, SrcRow, DestCol, DestRow, Board, Flag):-
 	makeMove(Board, SrcCol, SrcRow, DestCol, DestRow, TempBoard), !,
 	checkForCheck(TempBoard, Flag).
 
-differentPositions(SrcCol, SrcRow, DestCol, DestRow, _):-
+differentPositions(SrcCol, SrcRow, DestCol, DestRow, Board, _):-
 	SrcRow =\= DestRow ; SrcCol =\= DestCol.
 
-differentPositions(_, _, _, _, Flag):-
-	invalidMove(Flag).
+differentPositions(_, _, _, _, Board, Flag):-
+	invalidMove(Board, Flag).
 
 differentColors(SrcCol, SrcRow, DestCol, DestRow, Board, _):-
 	getPiece(Board, SrcCol, SrcRow, PieceSrc),
@@ -224,12 +232,15 @@ differentColors(SrcCol, SrcRow, DestCol, DestRow, Board, _):-
 	getPieceColor(PieceDest, ColorDest),
 	ColorSrc \== ColorDest.
 
-differentColors(_, _, _, _, _, Flag):-
-	invalidMove(Flag).
+differentColors(_, _, _, _, Board, Flag):-
+	invalidMove(Board, Flag).
 
 %TODO: clearConsole and PrintBoard on failure
-invalidMove(Flag):-
+invalidMove(Board, Flag):-
 	Flag == 1,
+	write('Invalid!'), nl,
+	clearConsole,
+	printBoard(Board),
 	write('Invalid Move!'), nl,
 	pressEnterToContinue, !,
 	fail.
@@ -370,8 +381,8 @@ checkForJumping('Queen', SrcCol, SrcRow, DestCol, DestRow, Board, _):-
 checkForJumping('King', SrcCol, SrcRow, DestCol, DestRow, Board, _).
 checkForJumping('Knight', SrcCol, SrcRow, DestCol, DestRow, Board, _).
 
-checkForJumping(_, _, _, _, _, _, Flag):-
-	invalidMove(Flag).
+checkForJumping(_, _, _, _, _, Board, Flag):-
+	invalidMove(Board, Flag).
 
 makeMove(Board, SrcCol, SrcRow, DestCol, DestRow, TempBoard):-
 	getPiece(Board, SrcCol, SrcRow, Piece),
@@ -385,8 +396,8 @@ checkForCheck(TempBoard, Flag):-
 	\+(makePseudoMoves('Black', TempBoard, WhiteKingCol, WhiteKingRow)),
 	\+(makePseudoMoves('White', TempBoard, BlackKingCol, BlackKingRow)).
 
-checkForCheck(_, Flag):-
-	invalidMove(Flag),
+checkForCheck(TempBoard, Flag):-
+	invalidMove(TempBoard, Flag),
 	fail.
 
 makePseudoMoves('Black', TempBoard, DestCol, DestRow):-
