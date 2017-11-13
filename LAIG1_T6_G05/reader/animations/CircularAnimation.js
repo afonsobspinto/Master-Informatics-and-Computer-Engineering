cd77a51ed8c50cbe655f65b262a73f119b5a4ba3
Tomás Oliveira <up201504746@fe.up.pt>
2017-11-13 22:22:42 +0000

    Circular Animation

--- LAIG1_T6_G05/reader/animations/CircularAnimation.js
+++ LAIG1_T6_G05/reader/animations/CircularAnimation.js
@@ -4,17 +4,31 @@ function CircularAnimation(scene, id, center, radius, inicialAngle, rotationAngl
   
   this.center = center;
   this.radius = radius;
-  this.inicialAngle = inicialAngle;
-  this.rotationAngle = rotationAngle;
+  this.inicialAngle = inicialAngle * Math.PI / 180;
+  this.rotationAngle = rotationAngle * Math.PI / 180;
   this.vel = vel;
   
-
+  this.lastCurrentTime = -1;
+  this.currentAngle = 0;
 }
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
 CircularAnimation.prototype.constructor = CircularAnimation;
 
 
-CircularAnimation.prototype.display = function() {
-
+CircularAnimation.prototype.update = function(currentTime) {
+	var delta, t;
+	var x, y, z;
+	
+	delta = (lastCurrentTime == -1) ? 0 : (currentTime - this.lastCurrentTime)/1000;
+	lastCurrentTime = currentTime;
+	
+	this.currentAngle = this.rotationAngle * currentTime;
+	
+	x = this.radius * Math.sin(this.initialAngle + this.currentAngle);
+    z = this.radius * Math.cos(this.initialAngle + this.currentAngle);
+	
+	this.scene.translate(x + this.center.x, this.center.y, z + this.center.z);
+    this.scene.rotate(this.initialAngle + this.currentAngle, 0, 1, 0);
+    this.scene.rotate(Math.PI / 2, 0, 1, 0);
 }
