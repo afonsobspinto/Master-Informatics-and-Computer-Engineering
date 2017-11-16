6afb4583b447cd817a423ac33831d65bd43bad4b
Tomás Oliveira <up201504746@fe.up.pt>
2017-11-16 19:21:06 +0000

    Circular Animation problem??

--- LAIG1_T6_G05/reader/animations/CircularAnimation.js
+++ LAIG1_T6_G05/reader/animations/CircularAnimation.js
@@ -4,12 +4,18 @@ function CircularAnimation(id, center, radius, initialAngle, rotationAngle, vel)
   
   this.center = center;
   this.radius = radius;
-  this.initialAngle = initialAngle * Math.PI / 180;
-  this.rotationAngle = rotationAngle * Math.PI / 180;
+  this.initialAngle = initialAngle * (Math.PI / 180) + Math.PI/2;
+  this.rotationAngle = rotationAngle * (Math.PI / 180);
   this.vel = vel;
-  
+
+  this.totalTime = 2*Math.PI*this.radius / this.vel;
+  this.velAng = this.rotationAngle / this.totalTime;
   this.lastCurrentTime = -1;
-  this.currentAngle = 0;
+  this.timePassed = 0;
+  
+  this.currentPosition = new Vector3(this.center.x + this.radius * Math.sin(this.initialAngle), this.center.y, this.center.z + this.radius * Math.cos(this.initialAngle));
+  this.currentAngle = this.initialAngle;
+  this.orientation = (this.rotationAngle < 0) ? (-Math.PI/2) : Math.PI/2;
 }
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
@@ -17,22 +23,34 @@ CircularAnimation.prototype.constructor = CircularAnimation;
 
 
 CircularAnimation.prototype.update = function(currentTime) {
-	var delta, t;
 	var x, y, z;
 	
-	delta = (this.lastCurrentTime == -1) ? 0 : (currentTime - this.lastCurrentTime)/1000;
+	if (!this.rendering)
+		return;
+
+	if (this.timePassed >= this.totalTime) {
+		x = this.center.x + this.radius * Math.sin(this.initialAngle+this.rotationAngle);
+		y = this.center.y;
+		z = this.center.z + this.radius * Math.cos(this.initialAngle+this.rotationAngle);
+		this.finished = true;
+        this.rendering = false;
+		return;
+	}
+	
+	if (this.lastCurrentTime != -1)
+		this.timePassed += (currentTime - this.lastCurrentTime) / 1000;
 	this.lastCurrentTime = currentTime;
 	
-	this.currentAngle = this.rotationAngle * currentTime;
+	this.currentAngle = this.velAng * this.timePassed + this.initialAngle;
 	
-	x = this.radius * Math.sin(this.initialAngle + this.currentAngle);
-    z = this.radius * Math.cos(this.initialAngle + this.currentAngle);
-	
-/*	this.scene.translate(x + this.center.x, this.center.y, z + this.center.z);
-    this.scene.rotate(this.initialAngle + this.currentAngle, 0, 1, 0);
-    this.scene.rotate(Math.PI / 2, 0, 1, 0);*/
+	x = this.center.x + this.radius * Math.sin(this.currentAngle);
+	y = this.center.y;
+	z = this.center.z + this.radius * Math.cos(this.currentAngle);
+	this.currentAngle += this.orientation;
+	console.log("x = ", x, " / y =  ", y, " / z = ", z, " / angle = ", this.currentAngle);
+	this.currentPosition = new Vector3(x,y,z);
 }
 
 
-CircularAnimation.prototype.getCurrentPosition = function() { };
-CircularAnimation.prototype.getCurrentAngle = function() { };
+CircularAnimation.prototype.getCurrentPosition = function() { return this.currentPosition; };
+CircularAnimation.prototype.getCurrentAngle = function() { return this.currentAngle; };
