72a1ce3d235cb92354a78d7c203031c2e69d3cfb
Afonso Pinto <up201503316@fe.up.pt>
2017-11-21 00:02:07 +0000

    remake

--- exercises/tests/TesteModelo.pl
+++ exercises/tests/TesteModelo.pl
@@ -15,132 +15,120 @@ performance(3788,[110,2,6,43]).
 performance(4865,[120,120,110,120]).
 performance(8937,[97,101,105,110]).
 
-%11
-impoeHelper(0, List, List).
-
-impoeHelper(N, List, L):-
-  impoe(N, List),
-  NewN is N-1,
-  impoeHelper(NewN, List, L).
-
 langford(N,L):-
-  ListSize is 2*N,
-  length(List, ListSize),
-  impoeHelper(N, List, L).
+	LSize is 2*N,
+  	length(Acc, LSize),
+	impoeAux(N, Acc, L).
+
+impoeAux(0, Acc, Acc).
+impoeAux(N, Acc, L):-
+	impoe(N,Acc),
+	NewN is N-1,
+	impoeAux(NewN, Acc, L).
 
 %10
-impoe(X,L) :- %Este predicado permite a geração de listas que seguem as restrições de espaço entre números (X) enunciadas
+impoe(X,L) :- %Este predicado permite a geração de listas que seguem as restrições de espaço entre números enunciadas. X representa o número em questão.
     length(Mid,X),
     append(L1,[X|_],L), append(_,[X|Mid],L1).
 
+
 %9
-
-predX(MaxAge,[ID|TailIds],[Performance|TailPerformance]) :- %Devolve Array de Performances cujos interpretes têm idade menor ou igual a Q
-    participant(ID,Age,Performance), Age=<MaxAge, !, %cut Verde
-    predX(MaxAge,TailIds,TailPerformance).
-predX(MaxAge,[ID|TailIds],TailPerformance) :-
+getPerformanceUnderMaxAge(MaxAge,[ID|IdsTail],[Performance|PerformanceTail]) :- %Obtem lista de performances com interpretes de idade não superior a MaxAge
+    participant(ID,Age,Performance), Age=<MaxAge, !, %Cut Verde, Nao altera soluções
+    getPerformanceUnderMaxAge(MaxAge,IdsTail,PerformanceTail).
+getPerformanceUnderMaxAge(MaxAge,[ID|IdsTail],PerformanceTail) :-
     participant(ID,Age,_), Age>MaxAge,
-    predX(MaxAge,TailIds,TailPerformance).
-predX(_,[],[]).
-
+    getPerformanceUnderMaxAge(MaxAge,IdsTail,PerformanceTail).
+getPerformanceUnderMaxAge(_,[],[]).
 
 %8
-eligibleOutcome(Id,Perf,TT) :-
+eligibleOutcome(Id,Perf,TT):-
     performance(Id,Times),
     madeItThrough(Id),
     participant(Id,_,Perf),
     sumlist(Times,TT).
 
 nextPhase(N, Participants):-
-  findall(TotalTime-ID-Name, eligibleOutcome(ID, Name, TotalTime), Result),
-  sort(Result, TempParticipants),
-  length(TempParticipants, Length),
-  (
-    Length < N -> !, fail;
-    Diff is Length - N
-  ),
-  cutBest(TempParticipants, Diff, Participants).
+	findall(TT-Id-Perf, eligibleOutcome(Id,Perf,TT), List),
+	sort(List, WorstFirst),
+	length(WorstFirst, Length),
+	Length >= N,
+	Diff is Length-N,
+	cutList(WorstFirst, Diff, Participants).
 
-cutBest(TempParticipants, 0, Participants):-
-  reverse(TempParticipants, Participants).
-
-cutBest([_|TempParticipants], Diff, Participants):-
-  NextDiff is Diff - 1,
-  cutBest(TempParticipants, NextDiff, Participants).
-
+cutList(List, 0, Participants):-
+	reverse(List,Participants).
+cutList([_|Tail], Diff, Participants):-
+	NewDiff is Diff -1,
+	cutList(Tail, NewDiff, Participants).
 
 %7
- juriFans(L):-
-   findall(Participant-Fan, (performance(Participant, Times), nth1(Fan, Times, 120)), Result),
-   findall(X-FanLists, (participant(X,_,_), performance(X, _), findall(Fa, member(X-Fa, Result), FanLists)), L).
-
+juriFans(JuriFansList):-
+	findall(Participant-ListFan, (performance(Participant, Times), findall(Fan, nth1(Fan, Times, 120), ListFan)), JuriFansList).
 
 %6
 nSuccessfulParticipants(T):-
-  findall(Participant, (performance(Participant, Times), sumlist(Times, Sum), Sum == 480), Result),
-  length(Result, T).
+	findall(Participant, (performance(Participant,Times), sumlist(Times, Sum), length(Times, Length), NoClick is Length * 120, Sum == NoClick), List),
+	length(List, T).
+
 
 %5
 allPerfs:-
-  performance(ID,Times),
-  participant(ID,_, Nome),
-  write(ID), write(':'), write(Nome), write(':'), write(Times), nl,
-  fail.
+	performance(Participant, Times),
+	participant(Participant, _, Performance),
+	write(Participant), write(:), write(Performance), write(:), write(Times), nl,
+	fail.
 allPerfs.
 
+
 %4
-bestParticipant(P1, P2, P) :-
-  performance(P1, Times1),
-  performance(P2, Times2),
-  sumlist(Times1, Score1),
-  sumlist(Times2, Score2),
-  (
-  Score1 > Score2 -> P = P1;
-  Score1 < Score2 -> P = P2;
-  fail
-  ).
+bestParticipant(P1, P2, Performance):-
+	performance(P1, Times1),
+	performance(P2, Times2),
+	sumlist(Times1, Sum1),
+	sumlist(Times2, Sum2),
+	Sum1 > Sum2,
+	Performance = P1.
 
-%3 Other Method maybe?
+bestParticipant(P1, P2, Performance):-
+	performance(P1, Times1),
+	performance(P2, Times2),
+	sumlist(Times1, Sum1),
+	sumlist(Times2, Sum2),
+	Sum1 < Sum2,
+	Performance = P2.
 
+%3
 patientJuri(JuriMember):-
-  bb_put(flag, 0),
-  performance(_, List),
-  nth1(JuriMember, List, Time),
-  bb_get(flag, Counter),
-  (
-  (Time == 120) -> NewCounter is Counter +1;
-  NewCounter is Counter
-  ),
-  bb_put(flag, NewCounter),
-  fail.
+	performance(Participant1, List1),
+	nth1(JuriMember, List1, Time1),
+	Time1 == 120,
+	performance(Participant2, List2),
+	nth1(JuriMember, List2, Time2),
+	Time2 == 120,
+	Participant1 \= Participant2.
 
-patientJuri(_):-
-  bb_get(flag, Counter),
-  Counter >= 2.
 
-%3 Tomas's Style
-patientJuri(JuriMember):-
-  performance(Participant1, Avaliation1),
-  performance(Participant2, Avaliation2),
-  Participant1 \= Participant2,
-  nth1(JuriMember, Avaliation1, 120),
-  nth1(JuriMember, Avaliation2, 120).
 
 
 %2
-juriTimes(Participant, JuriMember, Times, Total):-
-  juriTimesAux(Participant, JuriMember, [], Times, Total).
-juriTimesAux([], _, TempTimes, Times, Total):-
-  Times = TempTimes,
-  sumlist(Times, Total).
-juriTimesAux([H|Tail], JuriMember, TempTimes, Times, Total):-
-  performance(H, List),
-  nth1(JuriMember, List, Time),
-  append(TempTimes, [Time], NewTimes),
-  juriTimesAux(Tail, JuriMember, NewTimes, Times, Total).
+juriTimes(Participants, JuriMember, Times, Total):-
+	juriTimesAux(Participants,JuriMember, [], Times, Total).
+
+
+juriTimesAux([], _, Acc, Times, Total):-
+	Times = Acc,
+	sumlist(Times, Total).
+
+juriTimesAux([Head|Tail], JuriMember, Acc, Times, Total):-
+	performance(Head, List),
+	nth1(JuriMember, List, Time),
+	append(Acc, [Time], NewAcc),
+	juriTimesAux(Tail, JuriMember, NewAcc, Times, Total).
 
 
 %1
+
 madeItThrough(Participant):-
-  performance(Participant, Times),
-  member(120, Times).
+	performance(Participant, Times),
+	member(120, Times).
