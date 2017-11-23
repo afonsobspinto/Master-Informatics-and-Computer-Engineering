677f8d9ff714ef81d3fb14fb7dcf40ee2839eb2a
Tomás Oliveira <up201504746@fe.up.pt>
2017-11-23 21:10:07 +0000

    CircularAnimation  huge speed?

--- LAIG1_T6_G05/reader/animations/CircularAnimation.js
+++ LAIG1_T6_G05/reader/animations/CircularAnimation.js
@@ -1,8 +1,4 @@
-/**
- * CircularAnimation
- * @constructor
- */
-function CircularAnimation(id, center, radius, startang, rotang,speed) {
+function CircularAnimation(id, center, radius, startang, rotang, speed) {
     Animation.call(this, id);
 
     this.degToRad = Math.PI / 180;
@@ -13,34 +9,45 @@ function CircularAnimation(id, center, radius, startang, rotang,speed) {
     this.startang = startang * this.degToRad;
     this.rotang = rotang * this.degToRad;
 
+	this.totalDistance = Math.PI * 2 * this.radius;
     this.span = 2*Math.PI*this.radius / this.speed; // time
 
     this.angVelocity = this.rotang / this.span;
     this.transform = mat4.create();
 
+	this.accumulatedDistance = 0;
+    this.lastCurrentTime = -1;
     // v= 2*pi*r /t
 
 }
 
-CircularAnimation.prototype.update = function (currTime) {
+CircularAnimation.prototype.update = function (currentTime) {
     if (!this.rendering)
         return;
 
-    this.firstTime = this.firstTime || currTime;
-    let deltaTime = (currTime - this.firstTime) / 1000;   /* in seconds */
-    this.transform = mat4.create();
+    let delta = (this.lastCurrentTime === -1) ? 0 : (currentTime - this.lastCurrentTime)/1000;
+	this.lastCurrentTime = currentTime;
+	
 
-    if (deltaTime <= this.span) {     //TODO: Ending a little bit too soon.
-        let newTotalAngleDone = this.angVelocity * deltaTime;
-        mat4.translate(this.transform, this.transform, this.center.toArray());
+	this.accumulatedDistance += this.speed * delta; // Distance = velocity * time;
+	if(this.accumulatedDistance > this.totalDistance){
+            this.finished = true;
+            this.rendering = false;
+            mat4.rotate(this.transform, this.transform, 90 * this.degToRad, [0, 1, 0]);
+            mat4.translate(this.transform, this.transform, [0, 0, this.radius]);
+            mat4.rotate(this.transform, this.transform, this.startang + this.rotang, [0, 1, 0]);
+            mat4.translate(this.transform, this.transform, this.center.toArray());
+            
+        }
+	
+    else{     //TODO: Ending a little bit too soon.
+        let newTotalAngleDone = this.angVelocity * delta;
+        mat4.rotate(this.transform, this.transform, 90 * this.degToRad, [0, 1, 0]);
+        mat4.translate(this.transform, this.transform, [0, 0, this.radius]);
         mat4.rotate(this.transform, this.transform, this.startang + newTotalAngleDone, [0, 1, 0]);
-        mat4.translate(this.transform, this.transform, [0, 0, this.radius]);
-    } else {
         mat4.translate(this.transform, this.transform, this.center.toArray());
-        mat4.rotate(this.transform, this.transform, this.startang + this.rotang, [0, 1, 0]);
-        mat4.translate(this.transform, this.transform, [0, 0, this.radius]);
-        this.finished = true;
     }
+
     if(this.rotang < 0)
         mat4.rotate(this.transform, this.transform, 180*this.degToRad, [0, 1, 0]);
 };
