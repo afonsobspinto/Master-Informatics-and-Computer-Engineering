3d585b073a221cb002cc812854d87c58da64fd56
Afonso Pinto <up201503316@fe.up.pt>
2017-12-12 19:48:53 +0000

    path corrections

--- Project1/templates/common/header.php
+++ Project1/templates/common/header.php
@@ -26,7 +26,7 @@
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#team">Team</a></li>
-              <li><a href="action_login.php">Login</a></li>
+              <li><a href="pages/homeUser.php">Login</a></li>
               <li><a href="register.php">Register</a></li>
           </ul>
       </nav>
