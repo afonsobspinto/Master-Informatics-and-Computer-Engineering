e503eac330c50da204aaf3e4695c080d2461470d
Afonso Pinto <up201503316@fe.up.pt>
2017-12-13 02:11:42 +0000

    No path workaround initial page index

--- Project1/index.php
+++ Project1/index.php
@@ -5,11 +5,8 @@ include_once('includes/init.php');
 include_once('database/todoList.php');
 include_once('database/task.php');
 
-$tdLists = getAllTDLists();
-$tasks = getAllTasks();
 
 include_once('templates/common/header.php');
-include_once('templates/tdLists/list_tdLists.php');
-include_once('templates/tasks/list_tasks.php');
+include_once('templates/pages/home.php');
 include_once('templates/common/footer.php');
 ?>
