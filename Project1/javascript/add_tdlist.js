e13588e34ead26c7d3c68808429dc34199b74ab6
Afonso Pinto <up201503316@fe.up.pt>
2017-12-13 23:26:23 +0000

    ajax add

--- Project1/javascript/add_tdlist.js
+++ Project1/javascript/add_tdlist.js
@@ -1,7 +1,19 @@
-document.getElementById('add2dl').addEventListener('click', function() {
-    var value = document.getElementById('list').value;
-    if (value) {
-        console.log(value);
-    }
-});
+document.getElementById('add2dl').addEventListener('click', postList);
 
+function postList(e){
+    e.preventDefault();
+
+    var name = document.getElementById('list').value;
+    console.log(name);
+    var params = "name="+name;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'process.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onload = function(){
+        console.log(this.responseText);
+    }
+
+    xhr.send(params);
+}
