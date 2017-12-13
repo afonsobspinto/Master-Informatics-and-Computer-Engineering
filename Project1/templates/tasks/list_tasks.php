750af2f792e1c32a0999da86d0d72d3812f029ff
Tomás Oliveira <up201504746@fe.up.pt>
2017-12-13 10:38:13 +0000

    Update

--- Project1/templates/tasks/list_tasks.php
+++ Project1/templates/tasks/list_tasks.php
@@ -1,7 +1,7 @@
 <section id="tasks">
     <h2>Tasks</h2>
     <ul>
-        <div id="#addTask">
+        <div id="add">
             <li>
                 <input type="text" placeholder="Create a task..." id="item">
                 <a><img src="images/site/add.svg" alt="Add"></a>
@@ -10,7 +10,7 @@
         <?php foreach ($tasks as $task) { ?>
             <li>
                 <a class="description"><?=$task['tsk_description']?></a>
-                <a href="#remove">
+                <a href="#removeTask">
                     <img src="images/site/delete.svg" alt="Delete">
                 </a>
             </li>
--- Project1/templates/tdLists/list_tdLists.php
+++ Project1/templates/tdLists/list_tdLists.php
@@ -1,7 +1,7 @@
 <section id="tdLists">
   <h2>To Do Lists</h2>
   <ul>
-      <div id="#addList">
+      <div id="add">
           <li>
               <input type="text" placeholder="Create a list..." id="list">
               <a><img src="images/site/add.svg" alt="Add"></a>
@@ -10,7 +10,7 @@
       <?php foreach ($tdLists as $tdList) { ?>
           <li>
               <a href="index.php?tdl_id=<?=$tdList['tdl_id']?>"><?=$tdList['tdl_name']?></a>
-              <a href="#remove">
+              <a href="#removeList">
                   <img src="images/site/delete.svg" alt="Delete">
               </a>
           </li>
