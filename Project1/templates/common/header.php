36d7c29d4832c656cb2d4c46c69e8057bea737d4
Tomás Oliveira <up201504746@fe.up.pt>
2017-12-14 07:19:50 +0000

    About e team no search

--- Project1/templates/common/header.php
+++ Project1/templates/common/header.php
@@ -27,8 +27,8 @@
                 <li><a href="listProfile.php"><?=$_SESSION['username']?></a></li>
                 <li><a href="action_logout.php">Logout</a></li>
             <?php } else { ?>
-                <li><a href="#about">About</a></li>
-                <li><a href="#team">Team</a></li>
+                <li><a href="index.php#about">About</a></li>
+                <li><a href="index.php#team">Team</a></li>
                 <li><a href="login.php">Login</a></li>
                 <li><a href="register.php">Register</a></li>
             <?php } ?>
