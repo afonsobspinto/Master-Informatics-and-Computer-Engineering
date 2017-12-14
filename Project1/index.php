6b9854708b3af37639fbd5e94a523e3160c7614a
Afonso Pinto <up201503316@fe.up.pt>
2017-12-14 02:21:40 +0000

    undo

--- Project1/index.php
+++ Project1/index.php
@@ -11,8 +11,6 @@ if (isset($_SESSION['username']) && $_SESSION['username'] != '') {
 /*    $tdLists = getAllUserTDLists($_SESSION['username']);*/
     $tasks = getAllTasks();
     $tdLists = getAllTDLists();
-    $userInfo = getAllUserInfo($_SESSION['username']);
-    print_r($userInfo);
     include_once($_SERVER["DOCUMENT_ROOT"].'/FEUP-LTW/Project1/templates/tdLists/list_tdLists.php');
     include_once($_SERVER["DOCUMENT_ROOT"].'/FEUP-LTW/Project1/templates/tasks/list_tasks.php');
 
