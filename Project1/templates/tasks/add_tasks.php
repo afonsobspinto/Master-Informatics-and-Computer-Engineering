a590091abaa786ff6cb210ebb47c7a8e08a06da5
Afonso Pinto <up201503316@fe.up.pt>
2017-12-14 09:28:57 +0000

    Merge branch 'master' of https://github.com/afonsobspinto/FEUP-LTW
    # Conflicts:
    #	Project1/javascript/updateTasks.js

---
+++ Project1/templates/tasks/add_tasks.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once($_SERVER["DOCUMENT_ROOT"].'/FEUP-LTW/Project1/includes/init.php');
+
+if(isset($_POST['name']) && isset($_POST[''])){
+
+
+    $name = htmlspecialchars($_POST['name']);
+
+    global $dbh;
+    $stmt = $dbh->prepare('SELECT usr_id FROM user WHERE usr_username = ?');
+    $stmt->execute(array($_SESSION['username']));
+    $id = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $stmt = $dbh->prepare('INSERT INTO todoList (tdl_name, tdl_cat, usr_id) VALUES (?,?,?)');
+    $stmt->execute(array($name, "NONE_YET", $id['usr_id']));
+
+}
