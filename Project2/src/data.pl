f9622770a2aa4417ae4b0b9acbf4dab606100e88
Afonso Pinto <up201503316@fe.up.pt>
2017-12-19 14:03:54 +0000

    change database structure

---
+++ Project2/src/data.pl
@@ -0,0 +1,48 @@
+subject(1, 'PLOG', 'Programming', 1, [1, 1], [2, 2, 2, 2, 2, 2]).
+subject(2, 'LAIG', 'Multimedia', 1, [2], [3, 3, 3, 3, 3, 3]).
+subject(3, 'ESOF', 'Engineering', 1, [2], [2, 2, 2, 2, 2, 2]).
+subject(4, 'LTW', 'Information', 1, [2], [2, 2, 2, 2, 2, 2]).
+subject(5, 'RCOM', 'Systems', 1, [2], [2, 2, 2, 2, 2, 2, 2]).
+subject(6, 'LBAW', 'Information', 2, [1, 1], [3, 3, 3, 3, 3, 3]).
+subject(7, 'IART', 'AI', 2, [3], [2, 2, 2]).
+subject(8, 'COMP', 'Programming', 2, [3, 1], [2, 2, 2]).
+subject(9, 'SDIS', 'Systems', 2, [2], [2, 2, 2, 2, 2, 2]).
+subject(10, 'PPIN', 'Social', 2, [2], [2, 2, 2]).
+
+
+teacher(1, 'HLC', 'Assistant', 'Programming', 0, _).
+teacher(2, 'RCS', 'Associated', 'Programming', 0, _).
+teacher(3, 'DCS', 'Assistant', 'Programming', 0, _).
+teacher(4, 'AAS', 'Associated', 'Multimedia', 0, _).
+teacher(5, 'AVC', 'Assistant', 'Multimedia', 0, _).
+teacher(6, 'JGB', 'Assistant', 'Multimedia', 0, _).
+teacher(7, 'RPR', 'Assistant', 'Multimedia', 0, _).
+teacher(8, 'AMA', 'Assistant', 'Engineering', 0, _).
+teacher(9, 'TBS', 'Assistant', 'Engineering', 0, _).
+teacher(10, 'NHF', 'Assistant', 'Engineering', 0, _).
+teacher(11, 'MPR', 'Associated', 'Systems', 0, _0).
+teacher(12, 'JMCN', 'Assistant', 'Systems', 0, _).
+teacher(13, 'MTA', 'Assistant', 'Systems', 0, _).
+teacher(14, 'SRC', 'Assistant', 'Systems', 0, _).
+teacher(15, 'AOR', 'Assistant', 'Information', 0, _).
+teacher(16, 'FFC', 'Assistant', 'Information', 0, _).
+teacher(17, 'JCL', 'Assistant', 'Information', 0, _).
+teacher(18, 'SSN', 'Assistant', 'Information', 0, _).
+teacher(19, 'ECO', 'Professor', 'AI', 0, _).
+teacher(20, 'APR', 'Assistant', 'AI', 0, _).
+teacher(21, 'JMPC', 'Professor', 'Programming', 0, _).
+teacher(22, 'RJFN', 'Assistant', 'Programming', 0, _).
+teacher(23, 'TDRC', 'Assistant', 'Programming', 0, _).
+teacher(24, 'PFS', 'Assistant', 'Systems', 0, _).
+teacher(25, 'HFC', 'Assistant', 'Systems', 0, _).
+teacher(26, 'MFST', 'Assistant', 'Social', 0, _).
+teacher(27, 'FMSTT', 'Assistant', 'Social', 0, _).
+
+
+scientificArea(1, 'Programming').
+scientificArea(2, 'Multimedia').
+scientificArea(3, 'Engineering').
+scientificArea(4, 'Systems').
+scientificArea(5, 'Information').
+scientificArea(6, 'AI').
+scientificArea(7, 'Social').
--- Project2/src/main.pl
+++ Project2/src/main.pl
@@ -1,17 +1,7 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
+:- consult('data.pl').
 
 
-:- include('example.pl').
-:- include('teacher.pl').
-:- include('subject.pl').
-
-main(Subjects, Teachers):-
-
-	write('Hello World'), nl, nl,
-	nth0(0, Subjects, Subject),
-	nth0(0, Teachers, Teacher),
-	printSubjectInfo(Subject),
-	nl, nl,
-	printTeacherInfo(Teacher),
-	nl, nl.
+main:-
+	write('Hello World').
