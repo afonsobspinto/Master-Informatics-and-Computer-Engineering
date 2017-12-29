cb3245baccbee55e84b960016cd2a7cdbcff35ba
Tomás Oliveira <up201504746@fe.up.pt>
2017-12-29 17:48:56 +0000

    Small Update

--- LAIG3_T6_G05/reader/racingKings/controller/Game.js
+++ LAIG3_T6_G05/reader/racingKings/controller/Game.js
@@ -3,13 +3,11 @@ class Game {
         this.scene = scene;
         this.client = new Client();
         this.board = new Board(scene);
-        this.counter = new Counter(scene);
         this.gameConfig = gameConfig;
         this.gameHistory = [];
     }
 
     display(){
         this.board.display();
-        this.counter.display();
     }
   }
