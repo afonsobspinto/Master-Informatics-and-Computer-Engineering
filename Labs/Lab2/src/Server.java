ca505e1cc645a55d4853e8f57844b1bade7f125a
Afonso Pinto <afonsobpinto@gmail.com>
2018-02-21 11:52:14 +0000

    correct last commit

--- Labs/Lab2/src/Server.java
+++ Labs/Lab2/src/Server.java
@@ -1,5 +1,5 @@
 public class Server {
-            public static void main(String[] args) {
-                System.out.println("Hello World");
-            }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
