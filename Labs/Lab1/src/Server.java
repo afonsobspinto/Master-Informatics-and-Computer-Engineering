ae0f6e6603cd4ea264ec6f900b38d7373b8f50fa
Afonso Pinto <afonsobpinto@gmail.com>
2018-02-21 12:34:03 +0000

    add server side

--- Labs/Lab1/src/Server.java
+++ Labs/Lab1/src/Server.java
@@ -1,23 +1,43 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public class Server {
+    private HashMap<String, String> licensPlates;
     private int portNumber;
     private DatagramSocket socket;
 
 
     Server(int portNumber) throws SocketException {
+        this.licensPlates = new HashMap<>();
         this.portNumber = portNumber;
         this.socket = new DatagramSocket(this.portNumber);
 
     }
 
-    public static void main(String[] args) throws SocketException {
+    public void receive() throws IOException {
+        byte[] buffer = new byte[256];
+        DatagramPacket receivePacket;
+
+        receivePacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(receivePacket);
+
+        String received = new String(receivePacket.getData());
+        System.out.println("Echoed Message: " + received);
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
         int portNumber = parseInput(args);
         if(portNumber > 0){
             Server server = new Server(portNumber);
+            while (true){
+                server.receive();
+            }
         }
         else{
             System.out.println("Usage: java Server <port_number>");
