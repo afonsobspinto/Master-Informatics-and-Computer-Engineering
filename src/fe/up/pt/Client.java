9b50faf719024ab708136a8c66f1d6d9e28ad57a
up201504746 <up201504746@B03AV0825.fe.up.pt>
2018-02-21 11:27:50 +0000

    Changes

--- src/fe/up/pt/Client.java
+++ src/fe/up/pt/Client.java
@@ -3,6 +3,6 @@ package fe.up.pt;
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
