65398970f706c87699d6da4b300515fa0b1dfd37
Afonso Pinto <afonsobpinto@gmail.com>
2018-02-21 13:02:05 +0000

    add client side (testing needed)

--- Labs/Lab1/src/Client.java
+++ Labs/Lab1/src/Client.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.regex.Pattern;
 
 public class Client {
@@ -6,13 +10,19 @@ public class Client {
     private String hostname;
     private int portNumber;
     private DatagramSocket socket;
-    private String oper;
-    private String plateNumber;
-    private String ownerName;
+    private String data;
 
-    Client(String[] args){
+    Client(String[] args) throws SocketException {
         parseInputs(args);
+        socket = new DatagramSocket();
 
+    }
+
+    public void sendRequest() throws IOException {
+        byte[] buffer = this.data.getBytes();
+        InetAddress address = InetAddress.getByName(this.hostname);
+        DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, address, this.portNumber);
+        socket.send(sendPacket);
     }
 
     //TODO: Make it crash when args are not correct
@@ -23,24 +33,25 @@ public class Client {
         if(isNumber){
             this.portNumber = Integer.parseInt(possibleNumber);
         }
-        this.oper = args[2];
+        this.data = args[2];
+
         String possiblePlateNumber = args[3];
         boolean isPlate = Pattern.matches("^(\\w{2}-?\\w{2}-?\\w{2})$", possiblePlateNumber);
         if(isPlate){
-            this.plateNumber = possiblePlateNumber;
+            this.data += possiblePlateNumber;
         }
-        this.ownerName = oper.equals("register") ? args[4] : null;
+        this.data += this.data.equals("register") ? args[4] : "";
 
-        System.out.println(this.hostname + this.portNumber + this.oper + this.plateNumber + this.ownerName);
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
         if (!(args.length == 4 || args.length == 5)) {
             System.out.println("Usage: java Echo <hostname> <port_number> <oper> <opnd>");
             return;
         }
 
         Client client = new Client(args);
+
     }
 }
