76ccfa802c396f40e5476fedd655d2c1bdf1ac49
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2018-03-15 12:46:01 +0000

    Grammar Complete

---
+++ src/SyntaxTestAll.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+
+ECHO Testing "array2_err.yal"
+java Parser ../yalErrorFiles/array2_err.yal
+PAUSE
+
+ECHO Testing "array4_err.yal"
+java Parser ../yalErrorFiles/array4_err.yal
+PAUSE
+
+ECHO Testing "aval1_err.yal"
+java Parser ../yalErrorFiles/aval1_err.yal
+PAUSE
+
+ECHO Testing "aval2_err.yal"
+java Parser ../yalErrorFiles/aval2_err.yal
+PAUSE
+
+ECHO Testing "aval3_err.yal"
+java Parser ../yalErrorFiles/aval3_err.yal
+PAUSE
+
+ECHO Testing "aval4_err.yal"
+java Parser ../yalErrorFiles/aval4_err.yal
+PAUSE
+
+ECHO Testing "aval5_err.yal"
+java Parser ../yalErrorFiles/aval5_err.yal
+PAUSE
+
+ECHO Testing "aval6_err.yal"
+java Parser ../yalErrorFiles/aval6_err.yal
+PAUSE
+
+ECHO Testing "aval7_err.yal"
+java Parser ../yalErrorFiles/aval7_err.yal
+PAUSE
+
+ECHO Testing "err1.yal"
+java Parser ../yalErrorFiles/err1.yal
+ECHO Finished testing
+PAUSE
---
+++ src/TestCorrectAll.bat
@@ -0,0 +1,62 @@
+@ECHO OFF
+
+ECHO Testing "array1.yal"
+java Parser ../yalFiles/array1.yal
+
+ECHO Testing "array2.yal"
+java Parser ../yalFiles/array2.yal
+
+ECHO Testing "aval1.yal"
+java Parser ../yalFiles/aval1.yal
+
+ECHO Testing "aval2.yal"
+java Parser ../yalFiles/aval2.yal
+
+ECHO Testing "aval3.yal"
+java Parser ../yalFiles/aval3.yal
+
+ECHO Testing "aval4.yal"
+java Parser ../yalFiles/aval4.yal
+
+ECHO Testing "aval5.yal"
+java Parser ../yalFiles/aval5.yal
+
+ECHO Testing "aval6.yal"
+java Parser ../yalFiles/aval6.yal
+
+ECHO Testing "aval7.yal"
+java Parser ../yalFiles/aval7.yal
+
+ECHO Testing "aval8.yal"
+java Parser ../yalFiles/aval8.yal
+
+ECHO Testing "library1.yal"
+java Parser ../yalFiles/library1.yal
+
+ECHO Testing "max.yal"
+java Parser ../yalFiles/max.yal
+
+ECHO Testing "max_array.yal"
+java Parser ../yalFiles/max_array.yal
+
+ECHO Testing "max1.yal"
+java Parser ../yalFiles/max1.yal
+
+ECHO Testing "maxmin.yal"
+java Parser ../yalFiles/maxmin.yal
+
+ECHO Testing "programa1.yal"
+java Parser ../yalFiles/programa1.yal
+
+ECHO Testing "programa2.yal"
+java Parser ../yalFiles/programa2.yal
+
+ECHO Testing "programa3.yal"
+java Parser ../yalFiles/programa3.yal
+
+ECHO Testing "sqrt.yal"
+java Parser ../yalFiles/sqrt.yal
+
+ECHO Finished testing
+PAUSE
+EXIT
