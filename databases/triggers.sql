4f07ed3ecac6d0f085a699ac574146877e921ee5
Cristiana Ribeiro <ribe.cristiana@gmail.com>
2018-04-03 18:29:30 +0100

    add trigger to add new user

--- databases/triggers.sql
+++ databases/triggers.sql
@@ -17,3 +17,33 @@ $$ LANGUAGE 'plpgsql';
 
 
 CREATE TRIGGER auctions_search_tsvector BEFORE INSERT OR UPDATE ON auctions FOR EACH ROW EXECUTE PROCEDURE auctions_search_update();
+
+-- check if new user has not null fields
+
+DROP TRIGGER IF EXISTS add_new_user ON users;
+CREATE OR REPLACE FUNCTION fn_add_new_user() RETURNS TRIGGER AS $$
+BEGIN
+  IF TG_OP = 'INSERT' THEN
+       IF NEW.username IS NULL THEN
+          RAISE EXCEPTION 'username cannot be null';
+       END IF;
+       IF NEW.first_name IS NULL THEN
+          RAISE EXCEPTION 'first_name cannot be null';
+       END IF;
+       IF NEW.last_name IS NULL THEN
+          RAISE EXCEPTION 'last_name  cannot be null';
+       END IF;
+       IF NEW.zip_code IS NULL THEN
+          RAISE EXCEPTION 'zip_code cannot be null';
+       END IF;
+       IF NEW.address IS NULL THEN
+          RAISE EXCEPTION 'address cannot be null';
+       END IF;
+  END IF;
+  RETURN NEW;
+END
+$$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER add_new_user BEFORE INSERT OR UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE fn_add_new_user();
+
