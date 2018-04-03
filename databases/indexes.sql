300eaa2a3fafb66a2497c3ef467ff55ae398281b
Ros <liandtow@gmail.com>
2018-04-03 20:24:27 +0100

    modify

--- databases/indexes.sql
+++ databases/indexes.sql
@@ -1,6 +1,8 @@
-DROP INDEX IF EXISTS username_users_idx;
 DROP INDEX IF EXISTS search_auctions_idx;
-
-CREATE INDEX username_users_idx ON users USING hash(username);
+DROP INDEX IF EXISTS auctions_category_idx;
+DROP INDEX IF EXISTS receiver_emails_idx;
 
 CREATE INDEX search_auctions_idx ON auctions USING GIN (search);
+CREATE INDEX auctions_category_idx ON auctions USING btree(category_id);
+
+CREATE INDEX receiver_emails_idx ON emails USING btree(receiver_id);
