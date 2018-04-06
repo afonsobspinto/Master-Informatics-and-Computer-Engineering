81622be2f7730b948dc0d8a8dbd685360de7210d
Afonso Pinto <afonsobpinto@gmail.com>
2018-04-06 18:49:52 +0100

    Hello Weka

---
+++ Project/src/DecisionTree.java
@@ -0,0 +1,11 @@
+import weka.*;
+
+public class DecisionTree {
+    public DecisionTree() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Weka! ");
+
+    }
+}
