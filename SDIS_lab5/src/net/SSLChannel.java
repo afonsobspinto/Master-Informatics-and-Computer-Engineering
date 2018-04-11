cac246f0cf37f6c066ee93f8b293aad1834845c7
leonardogomesc <up201503708@fe.up.pt>
2018-04-11 12:56:18 +0100

    Add files via upload

---
+++ SDIS_lab5/src/net/SSLChannel.java
@@ -0,0 +1,107 @@
+package net;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+public class SSLChannel {
+	
+	private SSLServerSocket serverSocket;
+	private SSLSocket socket;
+	private InetAddress ip;
+	private int port;
+	private final static int buffer_size = 64000; 
+	
+	public SSLChannel(String ip, int port){
+		try {
+			this.ip = InetAddress.getByName(ip);
+			this.port = port;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public SSLChannel(int port){
+		SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+		
+		try {
+			serverSocket = (SSLServerSocket) ssf.createServerSocket(port);
+			serverSocket.setNeedClientAuth(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean accept_connection(){
+		try {
+			socket = (SSLSocket) serverSocket.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean connect(){
+		try {
+			socket = (SSLSocket) new Socket(ip, port);
+			socket.startHandshake();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean disconnect(){
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean send(String message){
+		try {
+			socket.getOutputStream().write(message.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public String receive(){
+		byte[] data = new byte[buffer_size];
+		int bytes_read = 0;
+		
+		try {
+			bytes_read = socket.getInputStream().read(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+		
+		if(bytes_read == -1){
+			return "";
+		}
+		else{
+			data = Arrays.copyOfRange(data, 0, bytes_read);
+		}
+		
+		return new String(data);
+	}
+	
+}
