3ad5f323be61333864bd084d5676efeadae8157d
ros <ros65536@gmail.com>
2018-04-19 11:29:52 +0100

    db:seed complete

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,18 @@ class DatabaseSeeder extends Seeder
      {
          Eloquent::unguard();
 
+         $path = 'SQL_Files/database.sql';
+         DB::unprepared(file_get_contents($path));
+
+         $path = 'SQL_Files/indexes.sql';
+         DB::unprepared(file_get_contents($path));
+
+         $path = 'SQL_Files/triggers.sql';
+         DB::unprepared(file_get_contents($path));
+
          $path = 'SQL_Files/populate.sql';
          DB::unprepared(file_get_contents($path));
+
          $this->command->info('Database seeded!');
      }
 }
