e5c209caa10e01decf07e58d283a6fb496dc0059
Afonso Pinto <afonsobpinto@gmail.com>
2018-04-19 08:49:32 +0100

    fix register error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -78,7 +78,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
