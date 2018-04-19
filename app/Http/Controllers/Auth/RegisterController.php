27e1f7ada5bf3b5f26468890672368db0a5531b6
Afonso Pinto <afonsobpinto@gmail.com>
2018-04-19 10:26:59 +0100

    fix register error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -77,8 +77,9 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        print_r($data);
         return User::create([
-            'username' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
