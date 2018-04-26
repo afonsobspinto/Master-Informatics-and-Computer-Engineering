8fb3554e49fe39accfdbf85a986157a4b7d8fdd9
ros <ros65536@gmail.com>
2018-04-26 09:42:55 +0100

    added php outside AND correct upload image

--- upload_image.sh
+++ upload_image.sh
@@ -6,10 +6,10 @@ set -e
 DOCKER_USERNAME=ros65536 # Replace by your docker hub username
 IMAGE_NAME=lbaw1764
 
-# Modified to work from docker container
-docker exec lbaw_php composer install # Ensure that dependencies are available
-docker exec lbaw_php php artisan clear-compiled
-docker exec lbaw_php php artisan optimize
+# Ensure that dependencies are available
+composer install
+php artisan clear-compiled
+php artisan optimize
 
-docker build -t ros65536/lbaw1764 .
-docker push ros65536/lbaw1764
+docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
+docker push $DOCKER_USERNAME/$IMAGE_NAME
