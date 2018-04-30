d98db8d0f5ee53a952c787827e221603b6b15d0f
Toliveira97 <toliveira_97@hotmail.com>
2018-04-30 12:07:14 +0100

    8.2

--- Classes/Class8/linguagemnatural.pl
+++ Classes/Class8/linguagemnatural.pl
@@ -14,32 +14,31 @@ preposicao(_) --> [de].
 preposicao(s-f) --> [da].
 
 nome(p-m, rapazes) --> [rapazes].
-nome(s-m, rapaz) --> [rapaz].
+nome(s-m,rapaz) --> [rapaz].
 nome(s-m, rui) --> [rui].
 nome(s-m, luis) --> [luis].
 nome(s-f, rita) --> [rita].
 nome(s-f, ana) --> [ana].
-nome(s-f, maria) --> [maria].
-nome(s-m) --> [elefante].
-nome(p-m) --> [caes].
-nome(p-m) --> [gatos].
-nome(s-m) --> [cao].
-nome(s-m) --> [gato].
-nome(s-m) --> [futebol].
-nome(p-m) --> [morangos].
-nome(p-m) --> [amendoins].
-nome(p-m) --> [bolachas].
-nome(p-m) --> [humanos].
-nome(p-f) --> [pessoas].
-nome(s-m, rui) --> [rui].
+nome(s-f,maria) --> [maria].
+nome(s-m, elefante) --> [elefante].
+nome(p-m, caes) --> [caes].
+nome(p-m, gatos) --> [gatos].
+nome(s-m, cao) --> [cao].
+nome(s-m, gato) --> [gato].
+nome(s-m, futebol) --> [futebol].
+nome(p-m, morangos) --> [morangos].
+nome(p-m, amendoins) --> [amendoins].
+nome(p-m, bolachas) --> [bolachas].
+nome(p-m, humanos) --> [humanos].
+nome(p-f, pessoas) --> [pessoas].
 
 verbo(s, jogar, S) --> [joga], {humano(S)}.
 verbo(p, jogar, S) --> [jogam], {humano(S)}.
-verbo(s, jogar, S) --> [gosta], {humano(S)}.
-verbo(p, jogar, S) --> [gostam], {humano(S)}.
-verbo(s, jogar, S) --> [come], {humano(S)}.
-verbo(p, jogar, S) --> [comem], {humano(S)}.
-verbo(p, jogar, S) --> [sao], {humano(S)}.
+verbo(s, gostar, S) --> [gosta], {humano(S)}.
+verbo(p, gostar, S) --> [gostam], {humano(S)}.
+verbo(s, comer, S) --> [come], {humano(S)}.
+verbo(p, comer, S) --> [comem], {humano(S)}.
+verbo(p, ser, S) --> [sao], {humano(S)}.
 
 %%%
 % base de dados
@@ -71,12 +70,39 @@ comer(elefante, amendoim).
 ser(rui, rapaz).
 ser(X, humano) :- humano(X).
 
-frase(A,S,Ob) --> sn(N, S), sv(N, A, Ob, S).
-sn(N, S) --> determinante(N-G), nome(N-G, S).
+
+% 8.2
+
+%%%
+% gramática
+%
+
+pron_inter(_-_, ql) --> [quem].
+pron_inter(p-_, ql) --> [quais].
+pron_inter(p-m, qt) --> [quantos].
+pron_inter(p-f, qt) --> [quantas].
+
+pronome(_) --> [que].
+
+
+% 8.1
+frase(A, S, Ob) --> sn(N, S), sv(N, A, Ob,S).
+sn(N, S) --> determinante(N-G), nome(N-G,S).
 sn(N, S) --> nome(N-_, S).
-sv(N, gostar, Ob, S) --> verbo(N, gostar, S), {!}, preposicao(N-G), nome(N-G, Ob).
-sv(N, A, Ob, S) --> verbo(N, A, S), sn(_, Ob).
+sv(N, gostar, Ob, S) --> verbo(N, gostar, S), {!}, preposicao(N1-G1), nome(N1-G1, Ob).
+sv(N, A, Ob, S) --> verbo(N,A, S) , sn(_, Ob).
+
+
 concorda_frase(A, S, Ob):-
-    P =.. [A, S, Ob],
-    (P, !, write(concordo),
-    write(discordo)).
+					P =.. [A,S,Ob],
+					(P, !, write(concordo); write(discordo)).
+
+
+
+%8.2
+frase_i(Q, A, At, Ob) --> si(N, Q, At), sv(N,A,Ob,_).
+si(N, Q, At) --> pron_inter(N-G, Q), sni(N-G, At).
+si(N, Q, _) --> pron_inter(N-_, Q).
+
+sni(N-G, At) --> determinante(N-G), nome(N-G,At), [que].
+sni(N-G, At) --> nome(N-G,At).
