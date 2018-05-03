d829f3a4311288c84414553444d71a8dcecc1b22
311-311 <ribe.cristiana@gmail.com>
2018-05-03 11:28:01 +0100

    something

---
+++ app/Messages.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: cristiana
+ * Date: 03-05-2018
+ * Time: 9:19
+ */
+
+namespace App;
+
+
+class Messages
+{
+
+}
---
+++ resources/views/messages/show.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: cristiana
+ * Date: 03-05-2018
+ * Time: 10:51
+ */
