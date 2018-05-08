3e06facb78bd4b18a21170034a73afeb357ba330
ros <ros65536@gmail.com>
2018-05-08 01:21:16 +0100

    improve search sidebar form

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -11,82 +11,47 @@
 
 @section('content')
     {{-- Sidebar --}}
-    <div id="sidebar">
-        <ul class="sidebar-nav ">
-            <div class="format">
-                <h5> Look for </h5>
+    <form id="sidebar" class="sidebar-nav ">
+        <div class="format">
+            <h5> Look for </h5>
+            <div class="radio">
+                <label><input type="radio" name="buy-type">All listings </label>
+                <label><input type="radio" name="buy-type">Auction </label>
+                <label><input type="radio" name="buy-type">Buy It Now</label>
+            </div>
+        </div>
 
-                <div class="radio">
-                    <label><input type="radio" name="optradio"> All listings </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio">Auction </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio">Buy It Now</label>
-                </div>
+        <div class="condition">
+            <h5> Condition </h5>
+            <div class="radio">
+                <label><input type="radio" name="condition"> New </label>
+                <label><input type="radio" name="condition"> Used </label>
+                <label><input type="radio" name="condition"> Not Specified </label>
+            </div>
+        </div>
+
+        <div class="price">
+            <h5>   Price <i class="fas fa-euro-sign"></i>   </h5>
+            <div>
+                <input class="col-xs-1" type="number" min="0"  placeholder="Min">
+                <br>
+                <input class="col-xs-1" type="number" min="0"  placeholder="Max">
             </div>
 
-            <div class="price">
-                <h5>   Price <i class="fas fa-euro-sign"></i>   </h5>
-                <div>
-                    <input class="col-xs-2" type="number" min="0"  placeholder="Min">
-                    <br>
-                    <input class="col-xs-2" type="number" min="0"  placeholder="Max">
-                </div>
+        </div>
 
+        <div class="location">
+            <h5> Item Location </h5>
+            <div class="radio">
+                <label><input type="radio" name="country"> BidBay </label>
+                <label><input type="radio" name="country"> Asia </label>
+                <label><input type="radio" name="country">Africa </label>
+                <label><input type="radio" name="country">America</label>
+                <label><input type="radio" name="country">Europe</label>
+                <label><input type="radio" name="country">Australia</label>
             </div>
-
-            <div class="category">
-                <h5> Categories </h5>
-                <li>
-                    <a href="#">Technology</a>
-                </li>
-                <li>
-                    <a href="#">Clothes</a>
-                </li>
-                <li>
-                    <a href="#">Automobiles</a>
-                </li>
-            </div>
-
-            <div class="location">
-                <h5> Item Location </h5>
-                <div class="radio">
-                    <label><input type="radio" name="optradio"> BidBay </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio"> Asia </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio">Africa </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio">America</label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio">Europe</label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio">Australia</label>
-                </div>
-            </div>
-
-            <div class="condition">
-                <h5> Condition </h5>
-                <div class="radio">
-                    <label><input type="radio" name="optradio"> New </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio"> Used </label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio"> Not Specified </label>
-                </div>
-            </div>
-        </ul>
-
-    </div>
+        </div>
+    </form>
 
     {{-- page contents --}}
     <div id="main">
