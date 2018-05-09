f1707edd6450e4faa1fe06c3403a118510d73518
ros <ros65536@gmail.com>
2018-05-09 20:19:08 +0100

    fix upload image

--- docker_run.sh
+++ docker_run.sh
@@ -3,6 +3,9 @@ set -e
 
 env >> /var/www/.env
 php-fpm7.1 -D
+cd /var/www
 php artisan migrate:refresh
 php artisan db:seed
+rm public/storage -f
+ln -s /var/www/storage/app/public /var/www/public/storage
 nginx -g "daemon off;"
