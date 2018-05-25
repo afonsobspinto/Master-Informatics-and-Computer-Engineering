821e3cc721945de0a5599fc0c7a4d777389aee08
pc <pc@DESKTOP-CJCCF16.lan>
2018-05-25 19:32:00 +0100

    update

--- Raft/Raft.java
+++ Raft/Raft.java
@@ -29,7 +29,7 @@ public class Raft<T> {
 	private class SocketCommunicator implements Runnable {
 		private Socket socket;
 
-		private SocketCommunicator(Socket socket) {
+		public SocketCommunicator(Socket socket) {
 			this.socket = socket;
 		}
 
@@ -53,12 +53,12 @@ public class Raft<T> {
 	private Long[] matchIndex;
 
 //	Required variables
-	private ServerSocket socket; // Maybe move this to the Listener class
+	private ServerSocket serverSocket; // Maybe move this to the Listener class
 	private AtomicBoolean running;
 	private ThreadPoolExecutor executor;
 
 	public Raft(int port) throws IOException {
-		this.socket = new ServerSocket(port);
+		this.serverSocket = new ServerSocket(port);
 		this.running = new AtomicBoolean(true);
 		this.executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
 	}
