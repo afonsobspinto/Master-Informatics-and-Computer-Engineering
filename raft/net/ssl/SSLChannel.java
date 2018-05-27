2c64e2a288fdca36f4a12c706e6c8127130b1c46
leonardogc <leonardogomescapozzi@gmail.com>
2018-05-27 17:20:26 +0100

    updated ssl channel functions

--- raft/net/ssl/SSLChannel.java
+++ raft/net/ssl/SSLChannel.java
@@ -15,7 +15,8 @@ public class SSLChannel {
 	private Socket socket;
 	private InetSocketAddress address;
 	private LinkedList<String> received = new LinkedList<>();
-
+	
+	private byte[] data;
 	private int BUFFER_SIZE;
 	
 	public SSLChannel(InetSocketAddress address){
@@ -43,6 +44,7 @@ public class SSLChannel {
 			socket = serverSocket.accept();
 		//	((SSLSocket) socket).startHandshake();
 			BUFFER_SIZE = socket.getReceiveBufferSize();
+			data = new byte[BUFFER_SIZE];
 		} catch (Exception e) {
 		//	e.printStackTrace();
 			return false;
@@ -56,6 +58,7 @@ public class SSLChannel {
 			socket = SSLSocketFactory.getDefault().createSocket(address.getHostString(), address.getPort());
 			((SSLSocket) socket).startHandshake();
 			BUFFER_SIZE = socket.getReceiveBufferSize();
+			data = new byte[BUFFER_SIZE];
 		} catch (Exception e) {
 		//	e.printStackTrace();
 			return false;
@@ -76,7 +79,12 @@ public class SSLChannel {
 	}
 	
 	public boolean send(String message) {
-		byte[] data = new String(Base64.getEncoder().encode(message.getBytes())).concat("\n").getBytes();
+		return send(message.getBytes());
+	}
+	
+	public boolean send(byte[] message) {
+		byte[] data = new String(Base64.getEncoder().encode(message)).concat("\n").getBytes();
+		
 		try {
 			socket.getOutputStream().write(data);
 		} catch (Exception e) {
@@ -87,9 +95,8 @@ public class SSLChannel {
 		return true;
 	}
 	
-	public String receive() {
+	public String receiveString() {
 		if (received.size() == 0) {
-			byte[] data = new byte[BUFFER_SIZE];
 			int bytes_read = 0;
 
 			try {
@@ -105,6 +112,26 @@ public class SSLChannel {
 			received = new LinkedList<>(Arrays.asList(new String(data, 0, bytes_read).split("\n")));
 		}
 		return new String(Base64.getDecoder().decode(received.removeFirst().getBytes()));
+	}
+	
+	public byte[] receive() {
+		if (received.size() == 0) {
+			int bytes_read = 0;
+
+			try {
+				bytes_read = socket.getInputStream().read(data);
+			} catch (Exception e) {
+			//	e.printStackTrace();
+			} finally {
+				if (bytes_read == -1) {
+					return null;
+				}
+			}
+
+			received = new LinkedList<>(Arrays.asList(new String(data, 0, bytes_read).split("\n")));
+		}
+		
+		return Base64.getDecoder().decode(received.removeFirst().getBytes());
 	}
 
 	public InetSocketAddress getRemoteAddress() {
