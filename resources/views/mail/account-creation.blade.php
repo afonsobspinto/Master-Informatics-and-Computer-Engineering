708ea5cdd6b28c590956c75e4def0f731fecc730
ros <ros65536@gmail.com>
2018-05-30 02:28:46 +0100

    improve email

--- resources/views/mail/account-creation.blade.php
+++
@@ -1,5 +0,0 @@
-
-Email confirmation message, please ignore. <br>
-Also please do not reply <br>
-
-Checkout the <a href="{{ url('/landing_page') }}">FAQ</a> if you have any questions.
--- resources/views/mail/registration-token.blade.php
+++ resources/views/mail/registration-token.blade.php
@@ -2,13 +2,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome Email</title>
+    <title>Bidbay: account email confirmation</title>
 </head>
 <body>
-    <h2>BidBay account email confirmation</h2>
+    <h2>BidBay: account email confirmation</h2>
 <br/>
-Please verify your email ({{$user['email']}}) to activate your account
+Hi {{$user['username']}}. Please verify your email ({{$user['email']}}) to activate your account.
 <br/>
-<a href="{{url('profile/verify', $user->verifyUser->token)}}">Verify Email</a>
+<a href="{{url('profile/verify', $user->verifyUser->token)}}">Verify Email</a> <br>
+<p>Please ignore if this email isn't meant for you.</p>
 </body>
 </html>
