549e99f3985825877fd12136b1c03ce670606f0e
aquelemiguel <miguelmanofernandes@gmail.com>
2018-10-09 16:45:05 +0100

    Adds a simple template flexbox for the activity screen

--- screens/SingleActivity.js
+++ screens/SingleActivity.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View } from 'react-native'
 import { ScreenOrientation } from 'expo'
 
 export default class SingleActivity extends Component {
   componentDidMount () {
-    ScreenOrientation.allow(ScreenOrientation.Orientation.LANDSCAPE)
+    ScreenOrientation.allow(ScreenOrientation.Orientation.LANDSCAPE_RIGHT)
   }
   componentWillUnmount () {
     ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT)
   }
   render () {
-    let pic = {
-      uri: 'http://bpic.588ku.com/element_pic/16/12/30/ac150585ba02eb068854fdcdc2bd2bb0.jpg'
-    }
     return (
-      <View>
-        <Text>Lavar os dentes</Text>
-        <Text>15 minutos</Text>
-        <Image source={pic} style={{ width: 200, height: 200 }} />
+      <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'grey' }} >
+        <View style={{ alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center', margin: 10, height: 70, backgroundColor: 'powderblue' }} >
+          <Text style={{ color: 'white', fontWeight: 'bold' }} >Progress Bar</Text>
+        </View>
+        <View style={{ flex: 1, flexDirection: 'row', margin: 10 }} >
+          <View style={{ width: 250, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red' }} >
+            <Text style={{ color: 'white', fontWeight: 'bold' }} >Toothbrush Image</Text>
+          </View>
+          <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'green' }} >
+            <View style={{ height: 60, alignItems: 'center', justifyContent: 'center', backgroundColor: 'blue' }} >
+              <Text style={{ color: 'white', fontWeight: 'bold' }} >Brush Teeth</Text>
+            </View>
+            <View style={{ height: 60, alignItems: 'center', justifyContent: 'center', backgroundColor: 'pink' }} >
+              <Text style={{ color: 'white', fontWeight: 'bold' }} >15 minutes</Text>
+            </View>
+            <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'violet' }} >
+              <View style={{ width: 125, alignItems: 'center', justifyContent: 'center', backgroundColor: 'orange' }} >
+                <Text style={{ color: 'white', fontWeight: 'bold' }} >Pause Icon</Text>
+              </View>
+              <View style={{ width: 125, alignItems: 'center', justifyContent: 'center', backgroundColor: 'teal' }} >
+                <Text style={{ color: 'white', fontWeight: 'bold' }} >Cancel Icon</Text>
+              </View>
+              <View style={{ width: 125, alignItems: 'center', justifyContent: 'center', backgroundColor: 'brown' }} >
+                <Text style={{ color: 'white', fontWeight: 'bold' }} >Complete Icon</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
