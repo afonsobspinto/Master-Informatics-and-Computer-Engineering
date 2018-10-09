89e7e1860476f88bdd1956807fc6d9480be24678
aquelemiguel <miguelmanofernandes@gmail.com>
2018-10-09 15:06:46 +0100

    Locks single activity screen orientation to landscape

--- screens/SingleActivity.js
+++ screens/SingleActivity.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View, Image } from 'react-native'
+import { ScreenOrientation } from 'expo'
 
 export default class SingleActivity extends Component {
+  componentDidMount () {
+    ScreenOrientation.allow(ScreenOrientation.Orientation.LANDSCAPE)
+  }
+  componentWillUnmount () {
+    ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT)
+  }
   render () {
     let pic = {
       uri: 'http://bpic.588ku.com/element_pic/16/12/30/ac150585ba02eb068854fdcdc2bd2bb0.jpg'
