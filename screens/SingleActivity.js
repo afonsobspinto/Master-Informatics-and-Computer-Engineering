7c7ef867537f554524e42cf4ed48d9614c1056cc
Sérgio Salgado <sergiodias.17@hotmail.com>
2018-10-10 18:07:30 +0100

    This commit adds the progress bar whose progression matches the current task duration given to the state, fetches the activity name and duration from the state (no longer handwritten in the render() function) and cleans some code related to the styles of the components

--- screens/SingleActivity.js
+++ screens/SingleActivity.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, StatusBar, StyleSheet } from 'react-native'
 import { ScreenOrientation } from 'expo'
 
+import AnimatedBar from 'react-native-animated-bar'
+
 export default class SingleActivity extends Component {
+  state = {
+    progress: 0,
+    taskDurationMinutes: 15,
+    taskTitle: 'Lavar os dentes',
+    color: 'grey',
+    updateRate: 0.5
+  }
   componentDidMount () {
     ScreenOrientation.allow(ScreenOrientation.Orientation.LANDSCAPE_RIGHT)
+
+    const interval = setInterval(() => {
+      if (this.state.progress >= 1) return clearInterval(interval)
+      if (this.state.progress > 0.2) this.state.color = 'green'
+      if (this.state.progress > 0.6 && this.state.progress < 0.85) this.state.color = 'orange'
+      if (this.state.progress > 0.85) this.state.color = 'red'
+
+      this.setState(state => {
+        return { progress: this.state.progress + this.state.updateRate / (this.state.taskDurationMinutes * 60) }
+      })
+    }, this.state.updateRate * 1000)
   }
   componentWillUnmount () {
     ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT)
   }
   render () {
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }} >
-        <View style={{ alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center', margin: 10, height: 70, backgroundColor: '#3F51B5' }} >
-          <Text style={{ color: 'white', fontWeight: 'bold' }} >Progress Bar</Text>
+      <View style={styles.flexColumn} >
+        <StatusBar hidden />
+        <View style={[{ alignSelf: 'stretch', margin: 10, height: 70, backgroundColor: '#3F51B5' }, styles.centerItem]} >
+          <AnimatedBar
+            progress={this.state.progress}
+            height={70}
+            borderColor='white'
+            barColor={this.state.color}
+            duration={50}
+          />
         </View>
         <View style={{ flex: 1, flexDirection: 'row', marginHorizontal: 10, marginBottom: 10 }} >
-          <View style={{ width: 250, alignItems: 'center', justifyContent: 'center', backgroundColor: '#C5CAE9' }} >
-            <Image style={{ width: 200, height: 200 }} source={require('../assets/images/act-brush-teeth.png')} />
+          <View style={[{ width: 250, backgroundColor: '#C5CAE9' }, styles.centerItem]} >
+            <Image style={styles.themeImage} source={require('../assets/images/act-brush-teeth.png')} />
           </View>
-          <View style={{ flex: 1, flexDirection: 'column' }} >
-            <View style={{ height: 60, alignItems: 'center', justifyContent: 'center', backgroundColor: '#7986CB' }} >
-              <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 30 }} >Brush Teeth</Text>
+          <View style={styles.flexColumn} >
+            <View style={[{ height: 70, backgroundColor: '#7986CB' }, styles.centerItem]} >
+              <Text style={styles.taskName} >{ this.state.taskTitle }</Text>
             </View>
-            <View style={{ height: 40, alignItems: 'center', justifyContent: 'center', backgroundColor: '#3F51B5' }} >
-              <Text style={{ color: 'white', fontSize: 15 }} >15 minutes</Text>
+            <View style={[{ height: 60, backgroundColor: '#3F51B5' }, styles.centerItem]} >
+              <Text style={styles.taskDuration} >{ this.state.taskDurationMinutes } minutos</Text>
             </View>
             <View style={{ flex: 1, flexDirection: 'row' }} >
-              <View style={{ width: 120, alignItems: 'center', justifyContent: 'center', backgroundColor: '#303F9F' }} >
-                <Image style={{ tintColor: 'white', width: 90, height: 90 }} source={require('../assets/images/nav-pause.png')} />
+              <View style={[{ flex: 0.95 }, styles.buttonBackground]} >
+                <Image style={styles.pauseButton} source={require('../assets/images/nav-pause.png')} />
               </View>
-              <View style={{ width: 125, alignItems: 'center', justifyContent: 'center', backgroundColor: '#303F9F' }} >
-                <Image style={{ tintColor: 'white', width: 80, height: 80 }} source={require('../assets/images/nav-cancel.png')} />
+              <View style={[{ flex: 1 }, styles.buttonBackground]} >
+                <Image style={styles.cancelButton} source={require('../assets/images/nav-cancel.png')} />
               </View>
-              <View style={{ width: 125, alignItems: 'center', justifyContent: 'center', backgroundColor: '#303F9F' }} >
-                <Image style={{ tintColor: 'white', width: 80, height: 80 }} source={require('../assets/images/nav-complete.png')} />
+              <View style={[{ flex: 1 }, styles.buttonBackground]} >
+                <Image style={styles.confirmButton} source={require('../assets/images/nav-complete.png')} />
               </View>
             </View>
           </View>
@@ -43,3 +70,47 @@ export default class SingleActivity extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  centerItem: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  themeImage: {
+    width: 200,
+    height: 200
+  },
+  taskName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 35
+  },
+  taskDuration: {
+    color: 'white',
+    fontSize: 18
+  },
+  flexColumn: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  buttonBackground: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#303F9F'
+  },
+  pauseButton: {
+    tintColor: 'white',
+    width: 100,
+    height: 90
+  },
+  cancelButton: {
+    tintColor: 'white',
+    width: 80,
+    height: 80
+  },
+  confirmButton: {
+    tintColor: 'white',
+    width: 80,
+    height: 80
+  }
+})
