b4623b33e114bbdc1cbcd1876d7ff1e0d9e74517
Sérgio Salgado <sergiodias.17@hotmail.com>
2018-10-13 01:28:44 +0100

    Adds progress bar time thresholds. Fixes interface formatting and code.

--- screens/SingleActivity.js
+++ screens/SingleActivity.js
@@ -7,50 +7,49 @@ import AnimatedBar from 'react-native-animated-bar'
 export default class SingleActivity extends Component {
   state = {
     progress: 0,
-    taskDurationMinutes: 15,
+    taskDurationMinutes: 1,
     taskTitle: 'Lavar os dentes',
     color: 'grey',
     updateRate: 0.5,
-    paused: false
+    isPaused: false
   }
   componentDidMount () {
     ScreenOrientation.allow(ScreenOrientation.Orientation.LANDSCAPE_RIGHT)
 
     const interval = setInterval(() => {
-
-      if(this.state.paused === true) return
+      if (this.state.isPaused === true) return
       if (this.state.progress >= 1) return clearInterval(interval)
       if (this.state.progress > 0.2) this.state.color = 'green'
-      if (this.state.progress > 0.6 && this.state.progress < 0.85) this.state.color = 'orange'
-      if (this.state.progress > 0.85) this.state.color = 'red'
+      if (this.state.progress >= 0.6 && this.state.progress < 0.85) this.state.color = 'orange'
+      if (this.state.progress >= 0.85) this.state.color = 'red'
 
-      this.setState(state => {
-        return { progress: this.state.progress + this.state.updateRate / (this.state.taskDurationMinutes * 60) }
-      })
+      if (!this.state.isPaused) {
+        this.setState(state => {
+          return { progress: this.state.progress + this.state.updateRate / (this.state.taskDurationMinutes * 60) }
+        })
+      }
     }, this.state.updateRate * 1000)
   }
   componentWillUnmount () {
     ScreenOrientation.allow(ScreenOrientation.Orientation.PORTRAIT)
   }
-  pauseActivity(){
-    this.setState(state => { return { paused: true }})
+  pauseActivity () {
+    this.setState(state => { return { isPaused: true } })
   }
-  resumeActivity(){
-    this.setState(state => { return { paused: false }})
+  resumeActivity () {
+    this.setState(state => { return { isPaused: false } })
   }
   render () {
-    if(this.state.paused === true){
+    if (this.state.isPaused === true) {
       return (
-        <View style={styles.centerItem}>
-          <View style={[{ alignSelf: 'stretch', margin: 10, height: 70, backgroundColor: '#3F51B5' }, styles.centerItem]} >
-          </View>
+        <View style={[{ flex: 1 }, styles.centerItem]}>
+          <StatusBar hidden />
           <TouchableOpacity onPress={() => this.resumeActivity()}>
             <Image style={styles.resumeButton} source={require('../assets/images/resume-button.png')} />
           </TouchableOpacity>
         </View>
-      );
-    }
-    else{
+      )
+    } else {
       return (
         <View style={styles.flexColumn} >
           <StatusBar hidden />
@@ -62,6 +61,11 @@ export default class SingleActivity extends Component {
               barColor={this.state.color}
               duration={50}
             />
+            <View style={styles.progressBarOverlay}>
+              <View style={[{ flex: 0.61 }, styles.progressBarDivider]} />
+              <View style={[{ flex: 0.26 }, styles.progressBarDivider]} />
+              <View style={{ flex: 0.14 }} />
+            </View>
           </View>
           <View style={{ flex: 1, flexDirection: 'row', marginHorizontal: 10, marginBottom: 10 }} >
             <View style={[{ width: 250, backgroundColor: '#C5CAE9' }, styles.centerItem]} >
@@ -75,31 +79,42 @@ export default class SingleActivity extends Component {
                 <Text style={styles.taskDuration} >{ this.state.taskDurationMinutes } minutos</Text>
               </View>
               <View style={{ flex: 1, flexDirection: 'row' }} >
-                <TouchableOpacity onPress={ () => this.pauseActivity() }>
-                  <View style={[{ flex: 0.95 }, styles.buttonBackground]} >
-                  <Image style={styles.pauseButton} source={require('../assets/images/nav-pause.png')} />
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={[{ flex: 1 }, styles.buttonBackground]} >
-                  <Image style={styles.cancelButton} source={require('../assets/images/nav-cancel.png')} />
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={[{ flex: 1 }, styles.buttonBackground]} >
-                  <Image style={styles.confirmButton} source={require('../assets/images/nav-complete.png')} />
-                  </View>
-                </TouchableOpacity>
+                <View style={[{ flex: 0.95 }, styles.buttonBackground]} >
+                  <TouchableOpacity onPress={() => this.pauseActivity()}>
+                    <Image style={styles.pauseButton} source={require('../assets/images/nav-pause.png')} />
+                  </TouchableOpacity>
+                </View>
+                <View style={[{ flex: 1 }, styles.buttonBackground]} >
+                  <TouchableOpacity>
+                    <Image style={styles.cancelButton} source={require('../assets/images/nav-cancel.png')} />
+                  </TouchableOpacity>
+                </View>
+                <View style={[{ flex: 1 }, styles.buttonBackground]} >
+                  <TouchableOpacity>
+                    <Image style={styles.confirmButton} source={require('../assets/images/nav-complete.png')} />
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </View>
         </View>
-      );
+      )
     }
   }
 }
 
 const styles = StyleSheet.create({
+  progressBarOverlay: {
+    flexDirection: 'row',
+    height: 70,
+    left: 0,
+    right: 0,
+    position: 'absolute'
+  },
+  progressBarDivider: {
+    borderRightColor: 'black',
+    borderRightWidth: 4
+  },
   centerItem: {
     alignItems: 'center',
     justifyContent: 'center'
