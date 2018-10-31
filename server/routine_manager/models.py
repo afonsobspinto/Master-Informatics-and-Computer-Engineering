cd7d3f9f7ab642681750815109da7d10dbf18795
Afonso Pinto <afonsobspinto@gmail.com>
2018-10-31 02:27:49 +0000

    Add basic models and migration

--- server/routine_manager/models.py
+++ server/routine_manager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Parent(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Child(models.Model):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
