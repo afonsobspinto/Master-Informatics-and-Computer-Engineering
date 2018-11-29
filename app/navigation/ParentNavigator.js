1ab6b36a088fcf7332a4a81cfea8872929dfc1db
aquelemiguel <miguelmanofernandes@gmail.com>
2018-11-29 05:17:35 +0000

    Remove obsolete home screen and correctly link screens

--- app/navigation/ParentNavigator.js
+++ app/navigation/ParentNavigator.js
@@ -1,14 +1,12 @@
 import { createStackNavigator } from 'react-navigation'
 
-import HomeScreen from '../screens/HomeScreen'
 import ChooseActivityScreen from '../screens/ChooseActivityScreen'
 import ChildMainMenuScreen from '../screens/ChildMainMenuScreen'
 import ParentScreen from '../screens/ParentScreen'
 
 export default createStackNavigator({
-  Home: HomeScreen,
-  ChooseActivityScreen: ChooseActivityScreen,
   ParentScreen: ParentScreen,
+  ChooseActivityScreen: ChooseActivityScreen,
   ChildMainMenu: ChildMainMenuScreen
 },
 {
