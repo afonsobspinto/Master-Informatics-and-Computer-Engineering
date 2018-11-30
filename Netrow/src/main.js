b5c13951096407a6527aaa57569b9d9d9434d423
Afonso Pinto <afonsobspinto@gmail.com>
2018-11-30 22:38:14 +0000

    Remove bootstrap

--- Netrow/src/main.js
+++ Netrow/src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma';
 import 'babel-polyfill';
-import BootstrapVue from 'bootstrap-vue'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import App from './App';
 import router from './router';
