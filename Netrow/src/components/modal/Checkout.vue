b81fddb0030f2b78ac088ef6f8b20031fae2aee8
rmcarvalho <rmcarvalho@users.noreply.github.com>
2018-12-01 11:53:14 +0000

    quantity is changeable from cart

--- Netrow/src/components/modal/Checkout.vue
+++ Netrow/src/components/modal/Checkout.vue
@@ -1,118 +1,155 @@
 <template>
-	<div :class="[ openModal ? 'is-active' : '', 'modal' ]">
-		<div class="modal-background"></div>
-		<div class="modal-card">
-			<header class="modal-card-head">
-				<p class="modal-card-title">{{ modalTitle }}</p>
-				<button class="delete" aria-label="close" @click="closeModal(false)"></button>
-			</header>
-			<section class="modal-card-body">
-				<div v-if="!isCheckoutSection">
-					<div class="box" v-for="product in products" :key="product.id">
-						<button class="is-pulled-right button is-info is-inverted" @click="removeFromCart(product.id)">{{ removeLabel }}</button>
-						<p>{{ product.title }}  {{ product.quantity > 0 ?  ` - Quantity: ${product.quantity}` : ''}}</p>
-						<p>{{ product.price }} &euro;</p>
-					</div>
-					<div v-if="products.length === 0">
-						<p>{{ cartEmptyLabel }}</p>
-					</div>
-				</div>
-				<div v-if="isCheckoutSection">
-					<p>You bought it :-)</p>
-				</div>
-			</section>
-			<footer class="modal-card-foot">
-				<button v-show="products.length > 0 && !isCheckoutSection" class="button is-success" @click="onNextBtn">{{ buyLabel }}</button>
-				<button v-if="isCheckoutSection" class="button is-success" @click="closeModal(true)">{{ closeLabel }}</button>
-			</footer>
-		</div>
-	</div>
+  <div :class="[ openModal ? 'is-active' : '', 'modal' ]">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ modalTitle }}</p>
+        <button class="delete" aria-label="close" @click="closeModal(false)"></button>
+      </header>
+      <section class="modal-card-body">
+        <div v-if="!isCheckoutSection">
+          <div class="box" v-for="product in products" :key="product.id">
+            <button
+              class="is-pulled-right button is-info is-inverted"
+              @click="removeFromCart(product.id)"
+            >{{ removeLabel }}</button>
+            <div>
+              <p style="display:inline-block; margin-right: 0.5em">{{ product.title }}</p>
+              <div class="select is-rounded is-small">
+                <select
+                  @change="onSelectQuantity(product.id, product.quantity)"
+                  v-model="product.quantity"
+                >
+                  <option
+                    v-for="quantity in quantityArray"
+                    :value="quantity"
+                    :key="quantity"
+                  >{{ quantity }}</option>
+                </select>
+              </div>
+            </div>
+            <p>{{ product.price }} &euro;</p>
+          </div>
+          <div v-if="products.length === 0">
+            <p>{{ cartEmptyLabel }}</p>
+          </div>
+        </div>
+        <div v-if="isCheckoutSection">
+          <p>You bought it :-)</p>
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <button
+          v-show="products.length > 0 && !isCheckoutSection"
+          class="button is-success"
+          @click="onNextBtn"
+        >{{ buyLabel }}</button>
+        <button
+          v-if="isCheckoutSection"
+          class="button is-success"
+          @click="closeModal(true)"
+        >{{ closeLabel }}</button>
+      </footer>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'checkout-component',
-    
-	data () {
-		return {
-			modalTitle: 'Checkout',
-			removeLabel: 'Remove from cart',
-			cartEmptyLabel: 'Your cart is empty',
-			closeLabel: 'Close',
-			isCheckoutSection: false
-		}
-	},
+  name: "checkout-component",
 
-	computed: {
-			products () {
-				return this.$store.getters.productsAdded;
-			},
-			openModal () {
-				if (this.$store.getters.isCheckoutModalOpen) {
-					return true;
-				} else {
-					return false;
-				}
-			},
-			buyLabel () {
-				let totalProducts = this.products.length,
-						productsAdded = this.$store.getters.productsAdded,
-						pricesArray = [],
-						productLabel = '',
-						finalPrice = '',
-						quantity = 1;
+  data() {
+    return {
+      modalTitle: "Checkout",
+      removeLabel: "Remove from cart",
+      cartEmptyLabel: "Your cart is empty",
+      closeLabel: "Close",
+      isCheckoutSection: false,
+      quantityArray: []
+    };
+  },
+  mounted() {
+    for (let i = 1; i <= 20; i++) {
+      this.quantityArray.push(i);
+    }
+  },
+  computed: {
+    products() {
+      return this.$store.getters.productsAdded;
+    },
+    openModal() {
+      if (this.$store.getters.isCheckoutModalOpen) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    buyLabel() {
+      let totalProducts = this.products.length,
+        productsAdded = this.$store.getters.productsAdded,
+        pricesArray = [],
+        productLabel = "",
+        finalPrice = "",
+        quantity = 1;
 
-				productsAdded.forEach(product => {
+      productsAdded.forEach(product => {
+        if (product.quantity >= 1) {
+          quantity = product.quantity;
+        }
 
-					if (product.quantity >= 1) {
-						quantity = product.quantity;
-					}
+        pricesArray.push(product.price * quantity); // get the price of every product added and multiply quantity
+      });
 
-					pricesArray.push((product.price * quantity)); // get the price of every product added and multiply quantity
-				});
+      finalPrice = pricesArray.reduce((a, b) => a + b, 0); // sum the prices
 
-				finalPrice = pricesArray.reduce((a, b) => a + b, 0); // sum the prices
-				
-				if (totalProducts > 1) { // set plural or singular
-					productLabel = 'products';
-				} else {
-					productLabel = 'product';
-				}
-				return `Buy ${totalProducts} ${productLabel} at ${finalPrice}€`;
-		},
-		isUserLoggedIn () {
-			return this.$store.getters.isUserLoggedIn;
-		}
-	},
+      if (totalProducts > 1) {
+        // set plural or singular
+        productLabel = "products";
+      } else {
+        productLabel = "product";
+      }
+      return `Buy ${totalProducts} ${productLabel} at ${finalPrice}€`;
+    },
+    isUserLoggedIn() {
+      return this.$store.getters.isUserLoggedIn;
+    }
+  },
 
-	methods: {
-		closeModal (reloadPage) {
-			this.$store.commit('showCheckoutModal', false);
+  methods: {
+    closeModal(reloadPage) {
+      this.$store.commit("showCheckoutModal", false);
 
-			if (reloadPage) {
-				window.location.reload();
-			}
-		},
-		removeFromCart (id) {
-			let data = {
-					id: id,
-					status: false
-			}
-			this.$store.commit('removeFromCart', id);
-			this.$store.commit('setAddedBtn', data);
-		},
-		onNextBtn () {
-			if (this.isUserLoggedIn) {
-				this.isCheckoutSection = true;
-			} else {
-				this.$store.commit('showCheckoutModal', false);
-				this.$store.commit('showLoginModal', true);
-			}
-		},
-		onPrevBtn () {
-			this.isCheckoutSection = false;
-		}
-	}
-}
+      if (reloadPage) {
+        window.location.reload();
+      }
+    },
+    removeFromCart(id) {
+      let data = {
+        id: id,
+        status: false
+      };
+      this.$store.commit("removeFromCart", id);
+      this.$store.commit("setAddedBtn", data);
+    },
+    onNextBtn() {
+      if (this.isUserLoggedIn) {
+        this.isCheckoutSection = true;
+      } else {
+        this.$store.commit("showCheckoutModal", false);
+        this.$store.commit("showLoginModal", true);
+      }
+    },
+    onPrevBtn() {
+      this.isCheckoutSection = false;
+    },
+    onSelectQuantity(id, quantity) {
+      let data = {
+        id: id,
+        quantity: quantity
+      };
+      this.$store.commit("quantity", data);
+    }
+  }
+};
 </script>
 
