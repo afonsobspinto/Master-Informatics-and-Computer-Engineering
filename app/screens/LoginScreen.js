a1f03614cfce22122b3927d90aa579669fc03d31
joaopscastro <jpedroc96@gmail.com>
2018-12-03 18:50:48 +0000

    Implement login screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -4,7 +4,7 @@ import { Container, Content, Form, Item, Input, Label, Button } from 'native-bas
 
 import styles from '../styles/ParentStyles/RegisterScreen.style'
 
-export default class RegisterScreen extends Component {
+export default class LoginScreen extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -17,37 +17,17 @@ export default class RegisterScreen extends Component {
     }
   }
 
-  validate (type, value) {
-    if (type === 'email') {
-      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      if (re.test(value)) {
-        this.setState(() => ({ emailError: false }))
-      } else {
-        this.setState(() => ({ emailError: true }))
-      }
-      this.setState(() => ({ emailHadInteraction: true }))
-    } else if (type === 'password') {
-      if (String(value).length < 6) {
-        this.setState(() => ({ passwordError: true }))
-      } else {
-        this.setState(() => ({ passwordError: false }))
-      }
-      this.setState(() => ({ passwordHadInteraction: true }))
-    }
-  }
-
   render () {
     return (
       <Container style={styles.registerContainter}>
         <StatusBar hidden />
         <Content contentContainerStyle={styles.contentContainter}>
-          <Text style={styles.registerTitle}>Registo de Conta</Text>
+          <Text style={styles.registerTitle}>Início de sessão</Text>
           <Form>
             <Item floatingLabel error={this.state.emailError} success={!this.state.emailError && this.state.emailHadInteraction} style={styles.inputContainer}>
               <Label style={styles.labelText}>E-mail</Label>
               <Input
                 className='email'
-                onChangeText={(text) => this.validate('email', text.trim())}
                 style={styles.labelText}
               />
             </Item>
@@ -56,20 +36,19 @@ export default class RegisterScreen extends Component {
               <Label style={styles.labelText}>Password</Label>
               <Input
                 className='password'
-                onChangeText={(text) => this.validate('password', text.trim())}
                 style={styles.labelText}
                 secureTextEntry
               />
             </Item>
 
             <Button rounded block primary style={styles.submitButton}>
-              <Text style={styles.buttonText}>Concluir Registo</Text>
+              <Text style={styles.buttonText}>Login</Text>
             </Button>
           </Form>
 
           <View style={styles.loginTextContainer}>
-            <Text style={styles.loginText}>Já possui uma conta?</Text>
-            <Text style={styles.loginRegisterText}>Faça login aqui!</Text>
+            <Text style={styles.loginText}>Não possui uma conta?</Text>
+            <Text style={styles.loginRegisterText} >Registe-se aqui</Text>
           </View>
         </Content>
       </Container>
