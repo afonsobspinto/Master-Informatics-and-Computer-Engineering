3e874c82ad4e27b9edbfd2d549dbd984d9694f80
Afonso Pinto <afonsobspinto@gmail.com>
2018-12-04 00:46:09 +0000

    Add prod and stage complete bash script and Improve .gitlab-ci

---
+++ deploy_prod.sh
@@ -0,0 +1,10 @@
+cd /home/ldsog02/t4g2/
+cat server/prod.env > server/.env
+cd /home/ldsog02/prod/server
+sudo docker-compose down
+cd /home/ldsog02
+sudo rm -rf /home/ldsog02/prod/server
+cp -r /home/ldsog02/t4g2/server /home/ldsog02/prod
+sudo rm -rf /home/ldsog02/t4g2
+cd /home/ldsog02/prod/server
+sudo docker-compose up
--- deploy_stage.sh
+++ deploy_stage.sh
@@ -1,8 +1,11 @@
-cd /home/ldsog02/prod/server
+cd /home/ldsog02/t4g2/
+cat server/staging.env > server/.env
+cat server/docker-compose_dev.yml server/docker-compose.yml
+cd /home/ldsog02/staging/server
 sudo docker-compose down
 cd /home/ldsog02
-sudo rm -rf /home/ldsog02/prod/server
-cp -r /home/ldsog02/t4g2/server /home/ldsog02/prod
+sudo rm -rf /home/ldsog02/staging/server
+cp -r /home/ldsog02/t4g2/server /home/ldsog02/staging
 sudo rm -rf /home/ldsog02/t4g2
-cd /home/ldsog02/prod/server
+cd /home/ldsog02/staging/server
 sudo docker-compose up
