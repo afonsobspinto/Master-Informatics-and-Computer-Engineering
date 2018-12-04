5850d4d5fc2ad8e25bd014e1bae94b0767147d83
Afonso Pinto <afonsobspinto@gmail.com>
2018-12-04 00:20:12 +0000

    Fix deploy_stage.sh

--- deploy_stage.sh
+++ deploy_stage.sh
@@ -1,8 +1,8 @@
 cd /home/ldsog02/prod/server
-docker-compose down
+sudo docker-compose down
 cd /home/ldsog02
-rm -rf /home/ldsog02/prod/server
-cp /home/ldsog02/t4g2/server /home/ldsog02/prod
-rm -rf /home/ldsog02/t4g2
+sudo rm -rf /home/ldsog02/prod/server
+cp -r /home/ldsog02/t4g2/server /home/ldsog02/prod
+sudo rm -rf /home/ldsog02/t4g2
 cd /home/ldsog02/prod/server
-docker-compose up
+sudo docker-compose up
