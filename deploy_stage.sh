c0f239ed55d3557576279cf1840741c51ba30ba7
Afonso Pinto <afonsobspinto@gmail.com>
2018-12-04 00:13:43 +0000

    Test deploy_stage.sh

--- deploy_stage.sh
+++ deploy_stage.sh
@@ -1,1 +1,8 @@
-echo '$1'
+cd /home/ldsog02/prod/server
+docker-compose down
+cd /home/ldsog02
+rm -rf /home/ldsog02/prod/server
+cp /home/ldsog02/t4g2/server /home/ldsog02/prod
+rm -rf /home/ldsog02/t4g2
+cd /home/ldsog02/prod/server
+docker-compose up
