cf3ba7c7060dc460b2d9ede358355a3b28c556d0
joaopscastro <jpedroc96@gmail.com>
2018-12-08 14:05:33 +0000

    Create tables for database

--- server/routine_manager/models.py
+++ server/routine_manager/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
 
-class Parent(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=200)
+    email = models.CharField(max_length=50) 
 
     def __str__(self):
         return self.name
 
 
 class Child(models.Model):
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    points = models.IntegerField(default=0)
+    level = models.IntegerField(default=0)
+    xp = models.IntegerField(default=0)
+    stars = models.IntegerField(default=0)
+    avatar = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
+
+class Routine(models.Model):
+    childID = models.ForeignKey(Child, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image = models.IntegerField(default=0)
+    photo = models.CharField(max_length=200)
+    color = models.IntegerField(defaul=0)
+    weight = models.IntegerField(defaul=0)
+    timeGoal = models.IntegerField(defaul=0)
+    timeMax = models.IntegerField(defaul=0)
+    timeMin = models.IntegerField(defaul=0)
