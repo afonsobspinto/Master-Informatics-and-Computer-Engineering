2a2622dc65f45fe0c9807598244a4a3dfc6a3761
Afonso Pinto <afonsobspinto@gmail.com>
2018-12-09 22:22:37 +0000

    Add register

--- server/routine_manager/views.py
+++ server/routine_manager/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+import json
 
 
 def index(request):
@@ -10,8 +11,11 @@ def index(request):
 @csrf_exempt
 def register(request):
     if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+
         try:
-            user = User.objects.create_user(email='lennon@thebeatles.com', password='johnpassword')
+            user = User.objects.create_user(email=body['email'], password=body['password'])
         except Exception:
             return JsonResponse({'status': '400'})
         return JsonResponse({'status': '200'})
