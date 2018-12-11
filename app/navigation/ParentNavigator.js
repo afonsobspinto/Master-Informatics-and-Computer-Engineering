47d936bef79f1e4d37a1ea0a463c3367010ff56a
aquelemiguel <miguelmanofernandes@gmail.com>
2018-12-11 03:21:29 +0000

    Add password prompt screen

--- app/navigation/ParentNavigator.js
+++ app/navigation/ParentNavigator.js
@@ -2,9 +2,11 @@ import { createStackNavigator } from 'react-navigation'
 
 import ParentMainMenuScreen from '../screens/parent/ParentMainMenuScreen'
 import ActivityFormScreen from '../screens/parent/ActivityFormScreen'
+import PasswordPromptScreen from '../screens/parent/PasswordPromptScreen'
 import RoutineFormScreen from '../screens/parent/RoutineFormScreen'
 
 export default createStackNavigator({
+  PasswordPromptScreen,
   ParentMainMenuScreen,
   ActivityFormScreen,
   RoutineFormScreen
---
+++ app/screens/parent/PasswordPromptScreen.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { StatusBar, Text, Alert } from 'react-native'
+import { Container, Content, Form, Item, Input, Label, Button, Icon } from 'native-base'
+import styles from '../../styles/ParentStyles/RegisterScreen.style'
+import PropTypes from 'prop-types'
+
+export default class PasswordPromptScreen extends Component {
+  verifyPassword = () => {
+    if (this.state.storedPW === this.state.inputPW) this.props.navigation.navigate('ParentMainMenuScreen')
+    else {
+      Alert.alert('Palavra-passe incorreta!', 'Tente novamente...', [
+        { text: 'Esqueci-me', onPress: () => console.log('Esqueceu-se lol burro') },
+        { text: 'OK', style: 'cancel' }
+      ],
+      { cancelable: false })
+    }
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      storedPW: 'asdf',
+      inputPW: '',
+      passwordError: null,
+      passwordHadInteraction: false,
+      PWIsSecure: true
+    }
+  }
+
+  render () {
+    return (
+      <Container style={styles.registerContainter}>
+        <StatusBar hidden />
+        <Content contentContainerStyle={styles.contentContainter}>
+          <Text style={styles.registerTitle}>Bloqueio parental</Text>
+          <Text style={styles.loginText}>Introduza a sua palavra-passe abaixo para aceder à zona de pai.</Text>
+          <Form>
+            <Item floatingLabel error={this.state.passwordError} success={!this.state.passwordError && this.state.passwordHadInteraction} style={styles.inputContainer}>
+              <Label style={styles.labelText}>Palavra-passe</Label>
+              <Input className='password' onChangeText={inputPW => this.setState({ inputPW })} style={styles.labelText} secureTextEntry={this.state.PWIsSecure} />
+            </Item>
+            <Button transparent onPress={() => this.setState({ PWIsSecure: !this.state.PWIsSecure })}>
+              <Icon name='eye' style={{ color: 'white' }} />
+            </Button>
+            <Button rounded block primary style={styles.submitButton} onPress={() => this.verifyPassword()}>
+              <Text style={styles.buttonText}>Entrar</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    )
+  }
+}
+
+PasswordPromptScreen.propTypes = {
+  navigation: PropTypes.object.isRequired
+}
