2245adf8e18dd445f56880f30814faafc6051240
Pipas_ <19pipas@gmail.com>
2018-12-11 17:37:00 +0000

    Fix layout problems in password screen

--- app/screens/parent/PasswordPromptScreen.js
+++ app/screens/parent/PasswordPromptScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StatusBar, Text, Alert } from 'react-native'
+import { View, Text, Alert } from 'react-native'
 import { Container, Content, Form, Item, Input, Label, Button, Icon } from 'native-base'
 import styles from '../../styles/ParentStyles/RegisterScreen.style'
 import PropTypes from 'prop-types'
@@ -30,18 +30,19 @@ export default class PasswordPromptScreen extends Component {
   render () {
     return (
       <Container style={styles.registerContainter}>
-        <StatusBar hidden />
         <Content contentContainerStyle={styles.contentContainter}>
           <Text style={styles.registerTitle}>Bloqueio parental</Text>
           <Text style={styles.loginText}>Introduza a sua palavra-passe abaixo para aceder à zona de pai.</Text>
           <Form>
-            <Item floatingLabel error={this.state.passwordError} success={!this.state.passwordError && this.state.passwordHadInteraction} style={styles.inputContainer}>
-              <Label style={styles.labelText}>Palavra-passe</Label>
-              <Input className='password' onChangeText={inputPW => this.setState({ inputPW })} style={styles.labelText} secureTextEntry={this.state.PWIsSecure} />
-            </Item>
-            <Button transparent onPress={() => this.setState({ PWIsSecure: !this.state.PWIsSecure })}>
-              <Icon name='eye' style={{ color: 'white' }} />
-            </Button>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+              <Item floatingLabel error={this.state.passwordError} success={!this.state.passwordError && this.state.passwordHadInteraction} style={styles.inputContainer}>
+                <Label style={styles.labelText}>Palavra-passe</Label>
+                <Input className='password' onChangeText={inputPW => this.setState({ inputPW })} style={styles.labelText} secureTextEntry={this.state.PWIsSecure} />
+              </Item>
+              <Button style={{ position: 'absolute', right: -10, bottom: -5 }} transparent onPress={() => this.setState({ PWIsSecure: !this.state.PWIsSecure })}>
+                <Icon name='eye' style={{ color: 'white' }} />
+              </Button>
+            </View>
             <Button rounded block primary style={styles.submitButton} onPress={() => this.verifyPassword()}>
               <Text style={styles.buttonText}>Entrar</Text>
             </Button>
