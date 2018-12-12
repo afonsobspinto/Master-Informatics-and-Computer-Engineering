9ec8687a2c32570a1a5c6a65c8b52a2a5c6fbe5a
Sérgio Salgado <sergiodias.17@hotmail.com>
2018-12-12 00:51:14 +0000

    Finish setting integration

--- server/routine_manager/views.py
+++ server/routine_manager/views.py
@@ -29,8 +29,19 @@ def login(request):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
         user = authenticate(username=body['email'], password=body['password'])
+        loggedUser = User.objects.get(username=body['email'])
+        settings = Settings.objects.get(user=loggedUser)
         if user is not None:
-            return JsonResponse({'status': '200'})
+            return JsonResponse({
+                'status': '200',
+                'activityProgressType': settings.activityProgressType,
+                'activityShowTimer': settings.activityShowTimer,
+                'activityFeedback': settings.activityFeedback,
+                'feedbackFrequency': settings.feedbackFrequency,
+                'visualStyle': settings.visualStyle,
+                'routinePlayType': settings.routinePlayType,
+                'playSounds': settings.playSounds
+            })
 
         else:
             return JsonResponse({'status': '400'})
@@ -63,3 +74,21 @@ def push_token(request):
         return JsonResponse({'status': '400'})
     return JsonResponse({'status': '200'})
 
+@csrf_exempt
+def settings(request):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        loggedUser = User.objects.get(username=body['userEmail'])
+        settings = Settings.objects.get(user=loggedUser)
+        settings.activityProgressType = body['activityProgressType']
+        settings.activityShowTimer = body['activityShowTimer']
+        settings.activityFeedback = body['activityFeedback']
+        settings.feedbackFrequency = body['feedbackFrequency']
+        settings.routinePlayType = body['routinePlayType']
+        settings.playSounds = body['playSounds']
+        try:
+            settings.save()
+        except Exception:
+            return JsonResponse({'status': '400'})
+        return JsonResponse({'status': '200'})
