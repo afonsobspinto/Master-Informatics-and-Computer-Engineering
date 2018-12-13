4f9953377bb2135700da0072e753af6518d5e14a
rmcarvalho <rmcarvalho@users.noreply.github.com>
2018-12-13 17:09:20 +0000

    Update Category.vue

--- Netrow/src/components/search/Category.vue
+++ Netrow/src/components/search/Category.vue
@@ -2,7 +2,7 @@
   <p class="control has-icons-right">
     <select class="input is-rounded" id="categoryInput" v-model="category" @change="selectCategory">
       <option value="all" selected>All categories</option>
-      <optgroup v-for="(subCat, cat) in getCategories" :key="cat" :label="cat">
+      <optgroup v-for="(subCat, cat) in categoriesList" :key="cat" :label="cat">
         <option  v-for="sub in subCat" :key="sub.subCategory" :value="sub.subCategory">{{sub.subCatDesc}}</option>
       </optgroup>
     </select>
@@ -36,7 +36,7 @@ export default {
 
     },
     getCategories: function () {
-      this.categoriesList = this.$store.getters.categories;      
+      return this.$store.getters.categories;      
       console.log('catlist')
       console.log(this.categoriesList)
       return this.categoriesList;
@@ -47,7 +47,37 @@ export default {
     selectCategory() {
       this.$store.commit("setCategorySelected", this.category);
     }
-  }
+  },
+  beforeRouteEnter(to, from, next) {
+        next(vm => {
+          HttpClient.instance(console.error, (instance) => {
+            instance.getCategories()
+              .then(catObj => {
+                let categories = catObj.DataSet.Table;
+                let organizedCategories = [];
+                categories.forEach(category => {
+                  if(organizedCategories[category.Descricao] === undefined)
+                  {
+                    organizedCategories[category.Descricao] = [];
+                  }
+                  organizedCategories[category.Descricao].push({
+                      subCategory: category.SubFamilia,
+                      subCatDesc: category.SubDescricao
+                  });
+                });
+
+                vm.$store.commit("setCategories", organizedCategories);
+                vm.categoriesList = organizedCategories;
+                console.log(organizedCategories)
+              })
+              .catch(e => {
+                console.error(e);
+                console.error("Failed to fetch categories data");
+                vm.$toaster.error("Failed to fetch categories data");
+              });
+            })
+        });
+      },
 };
 </script>
 
