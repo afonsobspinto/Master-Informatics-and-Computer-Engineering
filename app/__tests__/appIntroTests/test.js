4e137ebd55babc8a2b551cd6f36b94c380bca010
Rui Miguel Oliveira <ruimiguelalmeidaoliveira@gmail.com>
2018-12-15 12:20:02 +0000

    Resolve rebase conflicts

--- app/__tests__/appIntroTests/test.js
+++ app/__tests__/appIntroTests/test.js
@@ -4,7 +4,7 @@ import { configure, shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 import Adapter from 'enzyme-adapter-react-16'
 
-import AppIntroScreen from '../../../screens/parent/AppIntroScreen'
+import AppIntroScreen from '../../screens/AppIntroScreen'
 
 configure({ adapter: new Adapter() })
 
