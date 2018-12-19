c32acca07d141680b1126f1c43ae484dfe15febf
Toliveira97 <toliveira_97@hotmail.com>
2018-12-19 21:06:47 +0000

    Fix error in getting products

--- Netrow/src/components/checkout/CheckoutPage.vue
+++ Netrow/src/components/checkout/CheckoutPage.vue
@@ -5,143 +5,147 @@
         <h3 class="title">Order Details</h3>
         <br>
         <h4 class="subtitle">Shipping Info</h4>
-        <div class="columns is-multiline is-centered">
-            <div class="field column is-one-third">
-                <label class="label">Name</label>
-                <p class="control has-icons-left has-icons-right">
-                    <input
-                            :class="[highlightNameWithError ? 'input is-danger' : 'input']"
-                            type="text"
-                            v-model="profile.Nome"
-                            @keyup="checkNameOnKeyUp(name)"
-                    >
-                    <!-- <span class="icon is-small is-left">
-                      <i class="fas fa-user"></i>
-                    </span> -->
-                    <span v-if="highlightNameWithError !== null" class="icon is-small is-right">
-                    <i :class="[highlightNameWithError ? 'fas fa-exclamation-circle' : 'fas fa-check']"></i>
-                  </span>
-                </p>
-                <p v-if="highlightNameWithError" class="help is-danger">{{ nameErrorLabel }}</p>
-            </div>
-            <div class="field column is-one-third">
-                <label class="label">Email</label>
-                <p class="control has-icons-left has-icons-right">
-                    <input
-                            :class="[highlightEmailWithError ? 'input is-danger' : 'input']"
-                            type="email"
-                            name="emailName"
-                            v-model="profile.CDU_CampoVar1"
-                            @keyup="checkEmailOnKeyUp(email)"
-                            value="alexsmith@gmail.com"
-                    >
-                    <!-- <span class="icon is-small is-left">
-                      <i class="fas fa-envelope"></i>
-                    </span> -->
-                    <span v-if="highlightEmailWithError !== null" class="icon is-small is-right">
-                    <i :class="[highlightEmailWithError ? 'fas fa-exclamation-circle' : 'fas fa-check']"></i>
-                  </span>
-                </p>
-                <p v-if="highlightEmailWithError" class="help is-danger">{{ emailErrorLabel }}</p>
-            </div>
-            <div class="field column is-one-third">
-                <label class="label">Phone</label>
-                <div class="control">
-                    <label>
-                        <input class="input" type="number" v-model="profile.Telefone">
-                    </label>
+        <form @submit="checkForm" action="#" method="post">
+            <div v-if="isUserLoggedIn">
+                <div class="columns is-multiline is-centered">
+                    <div class="field column is-one-third">
+                        <label class="label">Name</label>
+                        <p class="control has-icons-left has-icons-right">
+                            <input
+                                    :class="[highlightNameWithError ? 'input is-danger' : 'input']"
+                                    type="text"
+                                    v-model="profile.Nome"
+                                    @keyup="checkNameOnKeyUp(name)"
+                            >
+                            <!-- <span class="icon is-small is-left">
+                              <i class="fas fa-user"></i>
+                            </span> -->
+                            <span v-if="highlightNameWithError !== null" class="icon is-small is-right">
+                            <i :class="[highlightNameWithError ? 'fas fa-exclamation-circle' : 'fas fa-check']"></i>
+                          </span>
+                        </p>
+                        <p v-if="highlightNameWithError" class="help is-danger">{{ nameErrorLabel }}</p>
+                    </div>
+                    <div class="field column is-one-third">
+                        <label class="label">Email</label>
+                        <p class="control has-icons-left has-icons-right">
+                            <input
+                                    :class="[highlightEmailWithError ? 'input is-danger' : 'input']"
+                                    type="email"
+                                    name="emailName"
+                                    v-model="profile.CDU_CampoVar1"
+                                    @keyup="checkEmailOnKeyUp(email)"
+                                    value="alexsmith@gmail.com"
+                            >
+                            <!-- <span class="icon is-small is-left">
+                              <i class="fas fa-envelope"></i>
+                            </span> -->
+                            <span v-if="highlightEmailWithError !== null" class="icon is-small is-right">
+                            <i :class="[highlightEmailWithError ? 'fas fa-exclamation-circle' : 'fas fa-check']"></i>
+                          </span>
+                        </p>
+                        <p v-if="highlightEmailWithError" class="help is-danger">{{ emailErrorLabel }}</p>
+                    </div>
+                    <div class="field column is-one-third">
+                        <label class="label">Phone</label>
+                        <div class="control">
+                            <label>
+                                <input class="input" type="number" v-model="profile.Telefone">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field column is-one-fifth">
+                        <label class="label">Address</label>
+                        <div class="control">
+                            <label>
+                                <input class="input" type="text" v-model="profile.Morada">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field column is-one-fifth">
+                        <label class="label">City</label>
+                        <div class="control">
+                            <label>
+                                <input class="input" type="text" v-model="profile.Localidade">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field column is-one-fifth">
+                        <label class="label">Zip-Code</label>
+                        <div class="control">
+                            <label>
+                                <input class="input" type="text" v-model="profile.CodigoPostal">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field column is-one-fifth">
+                        <label class="label">Country</label>
+                        <div class="control">
+                            <label>
+                                <input class="input" type="text" v-model="profile.Pais">
+                            </label>
+                        </div>
+                    </div>
+                    <div class="field column is-one-fifth">
+                        <label class="label">Fiscal Number</label>
+                        <div class="control">
+                            <label>
+                                <input class="input" type="number" v-model="profile.NumContribuinte">
+                            </label>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="field column is-one-fifth">
-                <label class="label">Address</label>
-                <div class="control">
-                    <label>
-                        <input class="input" type="text" v-model="profile.Morada">
-                    </label>
+                <br>
+                <div class="columns is-multiline is-centered">
+                    <div class="control column is-one-fifth"></div>
+                    <div class="control column is-one-third">
+                        <h4 class="subtitle">Payment Info</h4>
+                        <label class="label">Payment Options</label>
+                        <div class="select">
+                            <label>
+                                <select class="is-hovered" v-model="paymentOption">
+                                    <option>Bank Transfer</option>
+                                    <option>Credit Card</option>
+                                    <option>PayPal</option>
+                                    <option>Other</option>
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="column">
+                        <h4 class="subtitle">Your order</h4>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="product in products" :key="product.id">
+                                <th scope="row">1</th>
+                                <td>{{ product.title }}</td>
+                                <td>{{ product.quantity }}</td>
+                                <td>{{ product.price }} &euro;</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-            <div class="field column is-one-fifth">
-                <label class="label">City</label>
-                <div class="control">
-                    <label>
-                        <input class="input" type="text" v-model="profile.Localidade">
-                    </label>
+                <div class="column button is-offset-7">
+                <router-link :to="{ path: '/CheckoutPage', name: 'checkout-page-component' } ">
+                    <span>{{buyLabel}}</span>
+                </router-link>
                 </div>
+                <br>
             </div>
-            <div class="field column is-one-fifth">
-                <label class="label">Zip-Code</label>
-                <div class="control">
-                    <label>
-                        <input class="input" type="text" v-model="profile.CodigoPostal">
-                    </label>
-                </div>
+            <div>
+                <button class="column button is-success is-offset-10">Place Order</button>
             </div>
-            <div class="field column is-one-fifth">
-                <label class="label">Country</label>
-                <div class="control">
-                    <label>
-                        <input class="input" type="text" v-model="profile.Pais">
-                    </label>
-                </div>
-            </div>
-            <div class="field column is-one-fifth">
-                <label class="label">Fiscal Number</label>
-                <div class="control">
-                    <label>
-                        <input class="input" type="number" v-model="profile.NumContribuinte">
-                    </label>
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="columns is-multiline is-centered">
-            <div class="control column is-one-fifth"></div>
-            <div class="control column is-one-third">
-                <h4 class="subtitle">Payment Info</h4>
-                <label class="label">Payment Options</label>
-                <div class="select">
-                    <label>
-                        <select class="is-hovered" v-model="paymentOption">
-                            <option>Bank Transfer</option>
-                            <option>Credit Card</option>
-                            <option>PayPal</option>
-                            <option>Other</option>
-                        </select>
-                    </label>
-                </div>
-            </div>
-            <br>
-            <div class="column">
-                <h4 class="subtitle">Your order</h4>
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="product in products" :key="product.id">
-                        <th scope="row">1</th>
-                        <td>{{ product.title }}</td>
-                        <td>{{ product.quantity }}</td>
-                        <td>{{ product.price }} &euro;</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="column button is-offset-7">
-        <router-link :to="{ path: '/CheckoutPage', name: 'checkout-page-component' } ">
-            <span v-on:click="goToCheckout()">{{buyLabel}}</span>
-        </router-link>
-        </div>
-        <br>
-        <div class="column button is-offset-11">
-            <button>Place Order</button>
-        </div>
+        </form>
     </div>
 </template>
 
@@ -181,8 +185,8 @@
         },
 
         computed: {
-            products() {
-                return this.$store.getters.productsAdded;
+            isUserSignedUp () {
+                return this.$store.getters.isUserSignedUp;
             },
             buyLabel() {
                 let totalProducts = this.products.length,
@@ -212,20 +216,17 @@
             },
             isUserLoggedIn() {
                 return this.$store.getters.isUserLoggedIn;
+            },
+            products() {
+                return this.$store.getters.productsAdded;
             }
         },
 
         methods: {
-            closeModal(reloadPage) {
-                this.$store.commit("showCheckoutModal", false);
-
-                if (reloadPage) {
-                    window.location.reload();
-                }
-            },
             getProducts() {
                 let productsList = [];
-                for(i=0; i<this.products().length(); i++){
+                let i = 0;
+                for(i; i< this.products.size; i++){
                     productsList.push(
                         {
                             Artigo: this.products()[i].title,
@@ -234,6 +235,13 @@
                     )
                 }
                 return productsList;
+            },
+            closeModal(reloadPage) {
+                this.$store.commit("showCheckoutModal", false);
+
+                if (reloadPage) {
+                    window.location.reload();
+                }
             },
             getDate(day){
                 var today = new Date();
@@ -280,39 +288,7 @@
             checkForm (e) {
                 e.preventDefault();
 
-                if (this.name && this.email && isAnyObjectEmpty(this.profile)) {
-                    this.highlightEmailWithError = false;
-                    this.isFormSuccess = true;
-
-                    this.createUser()
-                        .then(data => {
-                            this.$store.commit('setUserName', this.name);
-                            this.$store.commit('isUserSignedUp', this.isFormSuccess);
-                            this.$store.commit('isUserLoggedIn', this.isFormSuccess);
-                            this.$store.commit('addUsername', this.profile.Cliente);
-                        }).catch(e => {
-                        console.error(e);
-                        this.$toaster.error("Failed to Update Profile.");
-                    });
-                } else {
-                    this.$toaster.error("Failed to register");
-                }
-
-                if (!this.name) {
-                    this.highlightNameWithError = true;
-                } else {
-                    this.highlightNameWithError = false;
-                }
-
-                if (!this.email) {
-                    this.highlightEmailWithError = true;
-
-                    if (this.email && !isValidEmail(this.email)) {
-                        this.emailErrorLabel = this.emailNotValidLabel;
-                    }
-                } else {
-                    this.highlightEmailWithError = false;
-                }
+                this.createOrder();
             },
             checkNameOnKeyUp (nameValue) {
                 if (nameValue) {
