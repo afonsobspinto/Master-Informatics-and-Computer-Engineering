05cf5053f2ffd56602b425a935b51dca09fdf00d
aquelemiguel <miguelmanofernandes@gmail.com>
2019-03-05 04:31:55 +0000

    Add player movement and collision on tilemap

---
+++ EngineersUnite/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController2D controller;
+    public Animator animator;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 30f;
+
+    bool jump, crouch = false;
+
+    // Update is called once per frame
+    void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump")) {
+            animator.SetBool("IsJumping", true);
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+
+    public void OnLanding() {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false; crouch = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Coin")) {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
