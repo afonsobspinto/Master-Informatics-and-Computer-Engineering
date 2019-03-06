33b24449cf705168d5d187f45c635352895bd2fd
aquelemiguel <miguelmanofernandes@gmail.com>
2019-03-06 23:08:09 +0000

    Implement one-way platform tilemap and drop on keydown

--- EngineersUnite/Assets/Scripts/CharacterController2D.cs
+++ EngineersUnite/Assets/Scripts/CharacterController2D.cs
@@ -13,7 +13,7 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;            // Whether or not the player is grounded.
+	public bool grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
@@ -43,8 +43,8 @@ public class CharacterController2D : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		bool wasGrounded = m_Grounded;
-		m_Grounded = false;
+		bool wasGrounded = grounded;
+		grounded = false;
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
@@ -53,7 +53,7 @@ public class CharacterController2D : MonoBehaviour
 		{
 			if (colliders[i].gameObject != gameObject)
 			{
-				m_Grounded = true;
+				grounded = true;
 				if (!wasGrounded)
 					OnLandEvent.Invoke();
 			}
@@ -74,7 +74,7 @@ public class CharacterController2D : MonoBehaviour
 		}
 
 		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
+		if (grounded || m_AirControl)
 		{
 
 			// If crouching
@@ -124,10 +124,10 @@ public class CharacterController2D : MonoBehaviour
 			}
 		}
 		// If the player should jump...
-		if (m_Grounded && jump)
+		if (grounded && jump)
 		{
 			// Add a vertical force to the player.
-			//m_Grounded = false;
+			grounded = false;
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
 		}
 	}
--- EngineersUnite/Assets/Scripts/PlayerMovement.cs
+++ EngineersUnite/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour 
 {
     public CharacterController2D controller;
+    private CharacterSwitcher charSwitchScript;
     public Animator animator;
 
-    float horizontalMove = 0f;
+    private float horizontalMove = 0f;
     public float runSpeed = 30f;
 
-    bool jump, crouch = false;
+    private bool jump, crouch = false;
 
-    private CharacterSwitcher charSwitchScript;
-
-    void Start() 
-    {
+    void Start() {
         charSwitchScript = (CharacterSwitcher) FindObjectOfType(typeof(CharacterSwitcher));
     }
 
-    void Update() 
-    {  
+    void Update() {  
         // Check whether this student is active.
         if (!name.Equals(this.charSwitchScript.getActiveCharacter()))
             return;
@@ -28,34 +25,35 @@ public class PlayerMovement : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if (Input.GetButtonDown("Jump")) 
-        {
+        if (Input.GetButtonDown("Jump")) {
             animator.SetBool("IsJumping", true);
             jump = true;
         }
 
         if (Input.GetButtonDown("Crouch"))
-            crouch = true;
-        else if (Input.GetButtonUp("Crouch"))
-            crouch = false;
+            FallThroughPlatform();
     }
 
-    public void OnLanding() 
-    {
+    public void FallThroughPlatform() {
+        gameObject.layer = LayerMask.NameToLayer("Platform");
+    }
+
+    public void OnLanding() {
         animator.SetBool("IsJumping", false);
     }
 
-    void FixedUpdate() 
-    {
+    void FixedUpdate() {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false; crouch = false;
     }
 
-    void OnTriggerEnter2D(Collider2D other) 
-    {
+    void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Coin")) 
-        {
             other.gameObject.SetActive(false);
-        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Platform"))
+            gameObject.layer = LayerMask.NameToLayer("Player");
     }
 }
