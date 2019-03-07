7ddde472fc0cb2db5a86dd1556d550f6a576c045
Toliveira97 <toliveira_97@hotmail.com>
2019-03-07 15:30:30 +0000

    Add Chemistry Demo Scene and exploding tube

---
+++ EngineersUnite/Assets/TubeHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeHandler : MonoBehaviour
+{
+
+    public Animator animator;
+    public PhysicsMaterial2D bouncyMaterial;
+    private BoxCollider2D boxCollider2D;
+
+    public void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && name == "Player (1)")
+        {
+                animator.SetBool("IsPowerOn", true);
+                TriggerChemistryAbility();
+    
+        }
+    }
+
+    private void TriggerChemistryAbility()
+    {
+        boxCollider2D.sharedMaterial = bouncyMaterial;
+    }
+
+}
