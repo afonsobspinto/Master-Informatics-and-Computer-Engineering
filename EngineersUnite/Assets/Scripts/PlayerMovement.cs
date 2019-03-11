79191cd458c2ebec586ab4566d7a7e9abd347432
Afonso Pinto <afonsobspinto@gmail.com>
2019-03-11 01:38:47 +0000

    Add basic freeze mechanic

--- EngineersUnite/Assets/Scripts/PlayerMovement.cs
+++ EngineersUnite/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour 
+public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     private CharacterSwitcher charSwitchScript;
@@ -14,11 +14,20 @@ public class PlayerMovement : MonoBehaviour
 
     private bool jump, crouch = false;
 
-    void Start() {
-        charSwitchScript = (CharacterSwitcher) FindObjectOfType(typeof(CharacterSwitcher));
+    private Rigidbody2D m_rigidbody2D;
+    private bool isfrozen = false;
+    private bool hasFrozenPowerUp = true;
+
+
+
+    void Start()
+    {
+        charSwitchScript = (CharacterSwitcher)FindObjectOfType(typeof(CharacterSwitcher));
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {  
+    void Update()
+    {
         // Check whether this student is active.
         if (!name.Equals(this.charSwitchScript.getActiveCharacter()))
             return;
@@ -26,39 +35,68 @@ public class PlayerMovement : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump"))
+        {
             animator.SetBool("IsJumping", true);
             jump = true;
         }
 
         if (Input.GetButtonDown("Crouch"))
+        {
             FallThroughPlatform();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            print("T pressed");
+
+            if (hasFrozenPowerUp)
+            {
+                if (isfrozen)
+                {
+                    hasFrozenPowerUp = false;
+                    m_rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+                }
+                else
+                {
+                    hasFrozenPowerUp = true;
+                    m_rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+                }
+                isfrozen = !isfrozen;
+            }
+        }
     }
 
-    public void FallThroughPlatform() {
+    public void FallThroughPlatform()
+    {
         gameObject.layer = LayerMask.NameToLayer("Platform");
     }
 
-    public void OnLanding() {
+    public void OnLanding()
+    {
         animator.SetBool("IsJumping", false);
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false; crouch = false;
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Coin")) 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
             other.gameObject.SetActive(false);
 
-        if (other.gameObject.CompareTag("Fire")) {
+        if (other.gameObject.CompareTag("Fire"))
+        {
             Debug.Log("Entered fire, restarting level.");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
-    private void OnCollisionStay2D(Collision2D other) {
+    private void OnCollisionStay2D(Collision2D other)
+    {
         if (other.gameObject.layer != LayerMask.NameToLayer("Platform"))
             gameObject.layer = LayerMask.NameToLayer("Player");
     }
