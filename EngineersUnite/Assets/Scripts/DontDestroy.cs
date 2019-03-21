04fd1dd918fa09fe7e5bb8ee8d3ced312a78ad9f
aquelemiguel <miguelmanofernandes@gmail.com>
2019-03-21 20:35:55 +0000

    Add background music to all levels

--- EngineersUnite/Assets/Scripts/DontDestroy.cs
+++ EngineersUnite/Assets/Scripts/DontDestroy.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour {
     private int deathCount = 0;
+    private AudioSource audioSource;
+    public AudioClip introMusic, levelMusic, endLevelMusic;
 
     void Awake() {
         DontDestroyOnLoad(this.gameObject);
+        this.audioSource = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        if (SceneManager.GetActiveScene().name == "IntroSlide1") {
+            audioSource.clip = this.introMusic;
+        }
+        else if (SceneManager.GetActiveScene().name == "MenuScene") {
+            audioSource.clip = this.levelMusic;
+        }
+        else if (SceneManager.GetActiveScene().name == "Level 99") {
+            audioSource.clip = this.endLevelMusic;
+        }
+        if (!audioSource.isPlaying) audioSource.Play();
     }
 
     public void incrementDeathCount() {
