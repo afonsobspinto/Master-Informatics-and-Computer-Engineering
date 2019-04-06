798d145b9939b87a5cdc587edd2224ca03ddee8a
rmcarvalho <rmcarvalho@users.noreply.github.com>
2019-04-06 16:00:55 +0100

    add HUD to screen with the bars

---
+++ Polymer Reef/Assets/CircleWaterQualityBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleWaterQualityBar : CircleUIBar
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _bar.fillAmount = 0;
+
+        //TODO Get values here
+        _actualValue = 0; //TODO replace value with some call to the water quality mechanic
+        _maxValue = 100.0f; //TODO replace value with some call to the water quality mechanic
+        _valueDecreaseStep = 4.5f; // TODO get water quality spent per second
+        _valueIncreaseStep = 20.0f; // TODO get/set water quality gained per second
+    }
+}
