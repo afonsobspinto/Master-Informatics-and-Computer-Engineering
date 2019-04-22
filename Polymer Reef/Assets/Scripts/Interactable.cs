add0d7756fd821bcf3c3d019421bf23858c51a6d
up201503708 <up201503708@fe.up.pt>
2019-04-22 19:47:15 +0100

    added power up objects collision detection

---
+++ Polymer Reef/Assets/Scripts/Interactable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float interactRadius = 1f;
+    public Transform player = null;
+    private bool hasInteracted = false;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!hasInteracted)
+        {
+            float distance = Vector3.Distance(transform.position, player.position);
+
+            if(distance <= interactRadius)
+            {
+                hasInteracted = true;
+                Interact();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactRadius);
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name + " and deleting it from the scene");
+        Destroy(gameObject);
+    }
+}
---
+++ Polymer Reef/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : Interactable
+{
+    public override void Interact()
+    {
+        Debug.Log("Interaction detected\nPlease implement interact method");
+        Destroy(gameObject);
+    }
+}
