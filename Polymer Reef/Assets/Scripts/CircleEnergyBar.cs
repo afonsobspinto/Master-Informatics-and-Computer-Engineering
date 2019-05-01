031d3fde823b1433c3dfa111f3c4c817b1c58535
Toliveira97 <toliveira_97@hotmail.com>
2019-05-01 19:11:30 +0100

    Light intensity increases as the energy circle bar

--- Polymer Reef/Assets/Scripts/CircleEnergyBar.cs
+++ Polymer Reef/Assets/Scripts/CircleEnergyBar.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class CircleEnergyBar : CircleUIBar
 {
+    public float ActualValue { get => _actualValue; set => _actualValue = value; }
+
     // Start is called before the first frame update
     void Start()
     {
         _bar.fillAmount = 0;
 
         //TODO Get values here
-        _actualValue = 0; //TODO replace value with some call to the health mechanic
+        ActualValue = 0; //TODO replace value with some call to the health mechanic
         _maxValue = 100.0f; //TODO replace value with some call to the energy mechanic
         _valueDecreaseStep = 4.5f; // TODO get energy spent per second
         _valueIncreaseStep = 20.0f; // TODO get/set energy gained per second (energy mechanics)
---
+++ Polymer Reef/Assets/Scripts/LightControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+    private GameObject energyCircle;
+    private CircleEnergyBar energyBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        energyCircle = GameObject.Find("EnergyBar");
+        energyBar = energyCircle.GetComponent<CircleEnergyBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform child in transform)
+        {
+            Light light = child.GetComponent<Light>();
+            float intens = light.intensity;
+            light.intensity = energyBar._actualValue/33;
+        }
+    }
+}
