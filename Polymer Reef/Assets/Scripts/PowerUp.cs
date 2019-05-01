e8f696629671f8397cb587f5f086d6963ebc5a70
Toliveira97 <toliveira_97@hotmail.com>
2019-05-01 14:28:06 +0100

    Sound working

--- Polymer Reef/Assets/Scripts/PowerUp.cs
+++ Polymer Reef/Assets/Scripts/PowerUp.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class PowerUp : Collectable
 {
+    [FMODUnity.EventRef]
+    public string selectsound;
+    FMOD.Studio.EventInstance soundevent;
+
+    void Start()
+    {
+        soundevent = FMODUnity.RuntimeManager.CreateInstance(selectsound);
+    }
+
     public override void Interact()
     {
         Debug.Log("Interaction detected\nPlease implement interact method");
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundevent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        PlaySound();
         Destroy(gameObject);
+    }
+
+    void PlaySound()
+    {
+        FMOD.Studio.PLAYBACK_STATE fmodPbState;
+        soundevent.getPlaybackState(out fmodPbState);
+        if(fmodPbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            soundevent.start();
     }
 }
