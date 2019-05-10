62e6b57339d835ca816a62f5076b4b09d1c29d0a
Toliveira97 <toliveira_97@hotmail.com>
2019-05-10 19:04:08 +0100

    Player health decreases when entering border

--- Polymer Reef/Assets/Scripts/CircleHealthBar.cs
+++ Polymer Reef/Assets/Scripts/CircleHealthBar.cs
@@ -11,7 +11,7 @@ public class CircleHealthBar : CircleUIBar
         _bar.fillAmount = 0;
 
         //TODO Get values here
-        _actualValue = 0; //TODO replace value with some call to the health mechanic
+        _actualValue = 100.0f; //TODO replace value with some call to the health mechanic
         _maxValue = 100.0f; //TODO replace value with some call to the health mechanic
         _valueDecreaseStep = 4.5f; // TODO get health spent per second
         _valueIncreaseStep = 20.0f; // TODO get/set health gained per second
