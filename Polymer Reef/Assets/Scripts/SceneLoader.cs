0ab897d5ea1cf634fe3e35a110391972f4503bde
up201503708 <up201503708@fe.up.pt>
2019-06-08 19:42:55 +0100

    add scene loader

---
+++ Polymer Reef/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int prevSceneIndex;
+    public int nextSceneIndex;
+
+    private bool insidePrevious = false;
+    private bool insideNext = false;
+    
+    public void setInsidePrevious(bool value)
+    {
+        this.insidePrevious = value;
+    }
+
+    public void setInsideNext(bool value)
+    {
+        this.insideNext = value;
+    }
+
+    public void loadScene()
+    {
+        if(insidePrevious)
+        {
+            Debug.Log("Load Next Scene");
+        }
+        else if(insideNext)
+        {
+            Debug.Log("Load Previous Scene");
+        }
+    }
+
+    public void unloadScene()
+    {
+        if (insidePrevious)
+        {
+            Debug.Log("Unload Next Scene");
+        }
+        else if (insideNext)
+        {
+            Debug.Log("Unload Previous Scene");
+        }
+    }
+}
---
+++ Polymer Reef/Assets/Scripts/SceneObjectsHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneObjectsHandler : MonoBehaviour
+{
+    public int prevSceneIndex;
+    public int nextSceneIndex;
+
+    private bool insidePrevious = false;
+    private bool insideNext = false;
+
+    public void setInsidePrevious(bool value)
+    {
+        this.insidePrevious = value;
+    }
+
+    public void setInsideNext(bool value)
+    {
+        this.insideNext = value;
+    }
+
+    public void changeActiveScene()
+    {
+        if (insidePrevious)
+        {
+            Debug.Log("Move Objects to Previous Scene");
+        }
+        else if (insideNext)
+        {
+            Debug.Log("Move Objects to Next Scene");
+        }
+    }
+}
