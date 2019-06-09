86d2bb429900eef5eb9124a643f59f920b0c41b9
up201503708 <up201503708@fe.up.pt>
2019-06-09 02:12:58 +0100

    updated logic

--- Polymer Reef/Assets/Scripts/SceneObjectsHandler.cs
+++ Polymer Reef/Assets/Scripts/SceneObjectsHandler.cs
@@ -22,12 +22,22 @@ public class SceneObjectsHandler : MonoBehaviour
 
     public void changeActiveScene()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
         if (insidePrevious)
         {
+            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneByBuildIndex(prevSceneIndex);
+
+            UnityEngine.SceneManagement.SceneManager.MoveGameObjectToScene(player, scene);
+            UnityEngine.SceneManagement.SceneManager.SetActiveScene(scene);
             Debug.Log("Move Objects to Previous Scene");
         }
         else if (insideNext)
         {
+            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneByBuildIndex(nextSceneIndex);
+            
+            UnityEngine.SceneManagement.SceneManager.MoveGameObjectToScene(player, scene);
+            UnityEngine.SceneManagement.SceneManager.SetActiveScene(scene);
             Debug.Log("Move Objects to Next Scene");
         }
     }
