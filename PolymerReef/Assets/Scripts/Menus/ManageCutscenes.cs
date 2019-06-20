1f7890a8c7429ac1a1c99fac1d6bfe4eb9c28492
Toliveira97 <toliveira_97@hotmail.com>
2019-06-20 14:18:25 +0100

    Add last cutscene going to credits

--- PolymerReef/Assets/Scripts/Menus/ManageCutscenes.cs
+++ PolymerReef/Assets/Scripts/Menus/ManageCutscenes.cs
@@ -6,12 +6,14 @@ using UnityEngine.Video;
 public class ManageCutscenes : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
-    private bool pressed = false;
+    private bool pressed = false, last = false;
 
     void Start()
     {
         videoPlayer = gameObject.GetComponent<VideoPlayer>();
         videoPlayer.loopPointReached += LoadScene;
+        if (gameObject.tag == "LastCutscene")
+            last = true;
     }
 
     private void Update()
@@ -25,12 +27,19 @@ public class ManageCutscenes : MonoBehaviour
 
     void LoadScene(VideoPlayer vp)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(1, UnityEngine.SceneManagement.LoadSceneMode.Additive).completed += OnMenuLoaded; ;
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(1, UnityEngine.SceneManagement.LoadSceneMode.Additive).completed += OnMenuLoaded;
     }
 
     private void OnMenuLoaded(AsyncOperation obj)
     {
-        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(0);
+        if(!last)
+            UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(0);
+        if(last)
+        {
+            UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(10);
+            GameObject.Find("Screens").transform.Find("CreditsScreen").gameObject.SetActive(true);
+            GameObject.Find("MainScreen").SetActive(false);
+        }
     }
  
 }
