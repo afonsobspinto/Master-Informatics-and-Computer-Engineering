04fab938770d2793c929b6a021244caca4dd2c38
Toliveira97 <toliveira_97@hotmail.com>
2019-12-17 15:16:46 +0100

    Fix settings error

--- src/twitter/twitter_data.py
+++ src/twitter/twitter_data.py
@@ -3,23 +3,24 @@ import re
 import tweepy as tw
 from src.settings import *
 
+
 def get_english_tweets(screen_name):
 
     auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tw.API(auth)
+    api = tw.API(auth, wait_on_rate_limit=True)
 
     all_tweets = []
 
     # Only allows 200 tweets at a time
-    new_tweets = api.user_timeline(screen_name = screen_name, count=200)
+    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
 
     all_tweets.extend(new_tweets)
 
     oldest = all_tweets[-1].id - 1
 
     while len(new_tweets) > 0:
-        new_tweets = api.user_timeline(screen_name = screen_name, count=200, max_id=oldest)
+        new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
 
         all_tweets.extend(new_tweets)
 
@@ -34,11 +35,12 @@ def get_english_tweets(screen_name):
             tweet.text = re.sub(r'http\S+', '', tweet.text)
             eng_tweets.append(tweet)
 
-    tweets_array = [[tweet.id_str, tweet.created_at, tweet.user.screen_name, tweet.text.encode('ascii', 'ignore').decode('ascii')] for tweet in eng_tweets]
+    tweets_array = [[tweet.id_str, tweet.created_at, tweet.user.screen_name,
+                     tweet.text.encode('ascii', 'ignore').decode('ascii')] for tweet in eng_tweets]
 
     with open('%s_tweets.csv' % screen_name, 'w') as f:
         writer = csv.writer(f)
-        writer.writerow(["id","created_at", "user", "text"])
+        writer.writerow(["id", "created_at", "user", "text"])
         writer.writerows(tweets_array)
 
     pass
@@ -46,4 +48,4 @@ def get_english_tweets(screen_name):
 
 if __name__ == '__main__':
     # Username account
-    get_english_tweets("nicktolhurst")
+    get_english_tweets("realdonaldTrump")
