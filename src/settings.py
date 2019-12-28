b7ee77c574098f7d3d4d58184262e09eaaba4d14
Afonso Pinto <afonsobspinto@gmail.com>
2019-12-28 16:22:04 +0000

    Update paths

--- src/settings.py
+++ src/settings.py
@@ -1,19 +1,20 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 DEBUG = True
 
-RAW_DATA = "../data/dataset.csv"
-#RAW_DATA = "../data/dataset_small.csv"
-CLEAN_DATA = "../data/pre_processed/clean_data.csv"
+RAW_DATA = "data/dataset.csv"
+# RAW_DATA = "../data/dataset_small.csv"
+CLEAN_DATA = "data/pre_processed/clean_data.csv"
 CONSUMER_KEY = os.getenv('CONSUMER_KEY')
 CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
-WORDCOUNT_IMG = "../data/pre_processed/wordcount_img.png"
-TOP_WORDS_IMG = "../data/pre_processed/topwords_img.png"
-MALLET_PATH = "../libraries/mallet-2.0.8/bin/mallet"
+WORDCOUNT_IMG = "data/pre_processed/wordcount_img.png"
+TOP_WORDS_IMG = "data/pre_processed/topwords_img.png"
+MALLET_PATH = "libraries/mallet-2.0.8/bin/mallet"
 
 PHASE = {
     "extractor": False,
@@ -24,7 +25,7 @@ PHASE = {
 
 USE_LAST_PARAMS = False
 PARAMS_PATH = "params.json"
-MODELS_PATH = "../data/processed/models"
+MODELS_PATH = "data/processed/models"
 
 # DEV
 EXTRACTED_DATASETS = "../../data/extracted"
