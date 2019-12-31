489859a135742034ce0ce9b83188a70cebdbe67a
Afonso Pinto <afonsobspinto@gmail.com>
2019-12-31 19:25:50 +0000

    Fix wordcloud

--- src/topic_modeling/topic_modeling.py
+++ src/topic_modeling/topic_modeling.py
@@ -202,28 +202,33 @@ class TopicModeling:
         return self.df_topic_keywords
 
     def save_word_cloud(self, num_topics):
-        cols = [color for name, color in mcolors.XKCD_COLORS.items()]
-        cloud = WordCloud(stopwords=ENGLISH_STOPWORDS,
-                          background_color='white',
-                          width=2500,
-                          height=1800,
-                          min_font_size=10,
-                          max_words=10,
-                          colormap='tab10',
-                          color_func=lambda *args, **kwargs: cols[i],
-                          prefer_horizontal=1.0)
+        pages = int(num_topics / 6)
         topics = self.mod.show_topics(formatted=False, num_topics=num_topics)
-        fig, axes = plt.subplots(int(num_topics/4),4, figsize=(10,10), sharex=True, sharey=True)
-        for i, ax in enumerate(axes.flatten()):
-            fig.add_subplot(ax)
-            topic_words = dict(topics[i][1])
-            cloud.generate_from_frequencies(topic_words, max_font_size=300)
-            plt.gca().imshow(cloud)
-            plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
-            plt.gca().axis('off')
 
-        plt.subplots_adjust(wspace=0, hspace=0)
-        plt.axis('off')
-        plt.margins(x=0, y=0)
-        plt.tight_layout()
-        plt.savefig(f"{self.save_path}/wordcloud.png")
+        index = 0
+        for i in range(0, pages):
+            cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+            cloud = WordCloud(stopwords=ENGLISH_STOPWORDS,
+                              background_color='white',
+                              width=2500,
+                              height=1800,
+                              min_font_size=10,
+                              max_words=10,
+                              colormap='tab10',
+                              color_func=lambda *args, **kwargs: cols[i],
+                              prefer_horizontal=1.0)
+            fig, axes = plt.subplots(3, 2, figsize=(10, 10), sharex=True, sharey=True)
+            for j, ax in enumerate(axes.flatten()):
+                fig.add_subplot(ax)
+                topic_words = dict(topics[index][1])
+                cloud.generate_from_frequencies(topic_words, max_font_size=300)
+                plt.gca().imshow(cloud)
+                plt.gca().set_title('Topic ' + str(index), fontdict=dict(size=16))
+                plt.gca().axis('off')
+                index += 1
+
+            plt.subplots_adjust(wspace=0, hspace=0)
+            plt.axis('off')
+            plt.margins(x=0, y=0)
+            plt.tight_layout()
+            plt.savefig(f"{self.save_path}/wordcloud{i}.png")
